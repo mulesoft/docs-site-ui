f4666deaef1b53f36c606896eec1a8481cf03ec5
Tony Li <tli415@gmail.com>
2019-07-16 15:17:02 -0700

    add gdpr banner

---
+++ src/js/06-gdpr.js
@@ -0,0 +1,19 @@
+;(function () {
+  'use strict'
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var gdprFlag = localStorage.getItem('GDPR')
+    var gdprEl = document.querySelector('.js-gdpr')
+    var gdprClose = document.querySelector('.js-gdpr-close')
+
+    function closeGDPR () {
+      localStorage.setItem('GDPR', 'off')
+      gdprEl.remove()
+    }
+
+    if (!gdprFlag) gdprEl.classList.add('show')
+
+    gdprClose.addEventListener('click', closeGDPR)
+    gdprClose.addEventListener('touchend', closeGDPR)
+  })
+})()
