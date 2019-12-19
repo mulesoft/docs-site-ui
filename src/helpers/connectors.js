444f5ca80a9ebbaba8e9e2d48fdbc7f47a245226
Dan Allen <dan@opendevise.com>
2019-12-19 03:23:46 -0700

    make templates for connectors landing dynamic; add required data to UI model

---
+++ src/helpers/connectors.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = function () {
+  return Object.values(this.site.components).filter((it) =>
+    it.latest.asciidocConfig.attributes['page-exchange-type'] === 'Connector'
+  )
+}
