fc82f8a52f716dfe365d45b4e253b102d4d70ab1
Dan Allen <dan@opendevise.com>
2019-12-20 04:00:19 -0700

    consolidate connector type down to single attribute

--- src/helpers/connectors.js
+++ src/helpers/connectors.js
@@ -1,7 +1,5 @@
 'use strict'
 
 module.exports = function () {
-  return Object.values(this.site.components).filter((it) =>
-    it.latest.asciidocConfig.attributes['page-exchange-type'] === 'Connector'
-  )
+  return Object.values(this.site.components).filter((it) => it.latest.asciidocConfig.attributes['page-connector-type'])
 }
