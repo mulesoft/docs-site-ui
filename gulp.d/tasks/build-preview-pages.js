0f184d475f8d326a97bbb4b9a71709d46c284065
Dan Allen <dan@opendevise.com>
2020-01-07 20:16:55 -0700

    reconfigure build to compute page model dynamically per page; simplify ui-model.yml

--- gulp.d/tasks/build-preview-pages.js
+++ gulp.d/tasks/build-preview-pages.js
@@ -38,33 +38,50 @@ module.exports = (src, previewSrc, previewDest, sink = () => map(), layouts = {}
           map((file, enc, next) => {
             const siteRootPath = path.relative(ospath.dirname(file.path), ospath.resolve(previewSrc))
             const uiModel = { ...baseUiModel }
-            uiModel.page = { ...uiModel.page }
             uiModel.siteRootPath = siteRootPath
             uiModel.siteRootUrl = path.join(siteRootPath, 'index.html')
             uiModel.uiRootPath = path.join(siteRootPath, '_')
             if (file.stem === '404') {
               uiModel.page = { layout: '404', title: 'Page Not Found' }
             } else {
+              const pageModel = uiModel.page = { ...uiModel.page }
               const doc = asciidoctor.load(file.contents, { safe: 'safe', attributes: ASCIIDOC_ATTRIBUTES })
-              uiModel.page.attributes = Object.entries(doc.getAttributes())
+              pageModel.attributes = Object.entries(doc.getAttributes())
                 .filter(([name, val]) => name.startsWith('page-'))
-                .reduce((accum, [name, val]) => {
-                  accum[name.substr(5)] = val
-                  return accum
-                }, {})
-              uiModel.page.layout = doc.getAttribute('page-layout', 'default')
-              uiModel.page.title = doc.getDocumentTitle()
-              uiModel.page.contents = Buffer.from(doc.convert())
-              // FIXME load a separate UI model for each page
-              if (file.stem === 'home') {
-                Object.assign(uiModel.page, {
-                  url: '/home.html',
-                  home: true,
-                  version: 'master',
-                  component: uiModel.site.components.general,
-                  componentVersion: uiModel.site.components.general.versions[0],
-                })
+                .reduce((accum, [name, val]) => ({ ...accum, [name.substr(5)]: val }), {})
+              pageModel.contents = Buffer.from(doc.convert())
+              pageModel.layout = doc.getAttribute('page-layout', 'default')
+              pageModel.title = doc.getDocumentTitle()
+              pageModel.url = '/' + file.relative.slice(0, -5) + '.html'
+              if (file.stem === 'home') pageModel.home = true
+              const componentName = pageModel.attributes['component-name'] || pageModel.src.component
+              const versionString = pageModel.attributes.version ||
+                (pageModel.attributes['component-name'] ? undefined : pageModel.src.version)
+              if (componentName) {
+                const component = pageModel.component = uiModel.site.components[componentName]
+                if (versionString) {
+                  pageModel.componentVersion = component.versions.find(({ version }) => version === versionString)
+                } else {
+                  pageModel.componentVersion = component.latest
+                }
+              } else {
+                pageModel.component = Object.values(uiModel.site.components)[0]
+                pageModel.componentVersion = pageModel.component.latest
               }
+              pageModel.module = 'ROOT'
+              pageModel.version = pageModel.componentVersion.version
+              pageModel.editUrl = pageModel.origin.editUrlPattern.replace('%s', file.relative)
+              pageModel.breadcrumbs = [{ content: pageModel.title, url: pageModel.url, urlType: 'internal' }]
+              pageModel.versions = pageModel.component.versions.map(({ version, displayVersion, url }, idx, arr) => {
+                const pageVersion = { version, displayVersion: displayVersion || version, url }
+                if (!idx) {
+                  pageVersion.latest = true
+                } else if (idx === arr.length - 1) {
+                  delete pageVersion.url
+                  pageVersion.missing = true
+                }
+                return pageVersion
+              })
             }
             file.extname = '.html'
             file.contents = Buffer.from(layouts[uiModel.page.layout](uiModel))
