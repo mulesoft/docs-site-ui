28f72dabfa3f4d474c838a5d3a98699a8a153a74
Gary Cheung <gcheung@mulesoft.com>
2022-07-13 12:46:39 -0700

    move js

---
+++ src/js/10-deprecation-notice.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict'
+
+  window.onscroll = function () {
+    makeSticky()
+  }
+  var depContainer = document.getElementsByClassName('deprecation-notice')[0]
+  var toolBar = document.getElementsByClassName('toolbar')[0]
+  var sticky = depContainer.offsetTop
+
+  function makeSticky () {
+    if (window.pageYOffset + toolBar.offsetHeight + 10 > sticky) {
+      depContainer.classList.add('sticky')
+    } else {
+      depContainer.classList.remove('sticky')
+    }
+  }
+})()
