16d6cec0cd39d56244eb96a2614891ab9d9270e5
Gary Cheung <gcheung@mulesoft.com>
2022-09-12 12:23:52 -0700

    add error handling

--- gulp.d/tasks/get-marketing-content.js
+++ gulp.d/tasks/get-marketing-content.js
@@ -12,9 +12,13 @@ module.exports = () => async () => {
 async function updateContent (component) {
   const content = await fetch(`https://www.mulesoft.com/api/${component}`)
   if (await isGoodStatus(content.status)) {
-    const body = await content.json()
-    if (await hasValidData(body)) {
-      fs.writeFileSync(`src/partials/${component}-content.hbs`, pretty(body.data))
+    try {
+      const body = await content.json()
+      if (await hasValidData(body)) {
+        fs.writeFileSync(`src/partials/${component}-content.hbs`, pretty(body.data))
+      }
+    } catch (error) {
+      console.warn('mulesoft endpoint returns invalid data. Keeping old version, no action is required.')
     }
   }
 }
