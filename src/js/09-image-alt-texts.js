7f8384f126af024a639b9eddfb319400792d3c89
Gary Cheung <gcheung@mulesoft.com>
2022-09-15 16:31:21 -0700

    add role

--- src/js/09-image-alt-texts.js
+++ src/js/09-image-alt-texts.js
@@ -5,21 +5,24 @@
 
   const externalLinks = document.querySelectorAll('[target="_blank"]')
   externalLinks.forEach(function (externalLink) {
-    const externalLinkImg = createImage('external-link')
-    externalLinkImg.alt = 'leaving the site'
-    externalLink.appendChild(externalLinkImg)
+    if (!externalLink.classList.contains('dw-playground-link')) {
+      const externalLinkImg = createLinkImage('external-link')
+      externalLinkImg.alt = 'leaving the site'
+      externalLink.appendChild(externalLinkImg)
+    }
   })
 
   const anchors = document.querySelectorAll('.anchor')
   anchors.forEach(function (anchor) {
-    const anchorImg = createImage('anchor')
+    const anchorImg = createLinkImage('anchor')
     const headerText = anchor.parentElement.textContent
     if (headerText) anchorImg.alt = `Jump to ${headerText}`
     anchor.appendChild(anchorImg)
   })
 
-  function createImage (element) {
+  function createLinkImage (element) {
     const img = document.createElement('img')
+    img.setAttribute('role', 'link')
     img.classList.add(`${element}-image`)
     img.src = `${uiRootPath}/img/icons/${element}.svg`
     return img
