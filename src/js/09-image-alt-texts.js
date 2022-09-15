94bba377c64217bc7ec6aaa6e038c6b8db793cf6
Gary Cheung <gcheung@mulesoft.com>
2022-09-15 16:14:22 -0700

    fix errors

--- src/js/09-image-alt-texts.js
+++ src/js/09-image-alt-texts.js
@@ -1,25 +1,25 @@
 ;(function () {
   'use strict'
 
-  let uiRootPath = document.getElementById('site-script').dataset.uiRootPath
-  
-  let externalLinks = document.querySelectorAll('[target="_blank"]')
+  const uiRootPath = document.getElementById('site-script').dataset.uiRootPath
+
+  const externalLinks = document.querySelectorAll('[target="_blank"]')
   externalLinks.forEach(function (externalLink) {
-    let externalLinkImg = createImage('external-link')
+    const externalLinkImg = createImage('external-link')
     externalLinkImg.alt = 'leaving the site'
     externalLink.appendChild(externalLinkImg)
   })
 
-  let anchors = document.querySelectorAll('.anchor')
+  const anchors = document.querySelectorAll('.anchor')
   anchors.forEach(function (anchor) {
-    let anchorImg = createImage('anchor')
-    let headerText = anchor.parentElement.textContent
-    if (headerText) anchorImg.alt = `Jump to ${headerText}` 
+    const anchorImg = createImage('anchor')
+    const headerText = anchor.parentElement.textContent
+    if (headerText) anchorImg.alt = `Jump to ${headerText}`
     anchor.appendChild(anchorImg)
   })
 
-  function createImage(element) {
-    let img = document.createElement('img')
+  function createImage (element) {
+    const img = document.createElement('img')
     img.classList.add(`${element}-image`)
     img.src = `${uiRootPath}/img/icons/${element}.svg`
     return img
