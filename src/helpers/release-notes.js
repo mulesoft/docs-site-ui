26944226de2cde09347229e99b41ffbcec76fb2c
Gary Cheung <gcheung@mulesoft.com>
2022-10-26 10:53:21 -0700

    clean up code and add version

--- src/helpers/release-notes.js
+++ src/helpers/release-notes.js
@@ -1,68 +1,60 @@
 module.exports = (numOfItems, { data }) => {
   const { contentCatalog, site } = data.root
   if (contentCatalog) {
-    const pages = contentCatalog.getPages(
-      ({ asciidoc, out }) => {
-        if (!out || !asciidoc) return
-        return isReleaseNotesWithRevdate(asciidoc)
-      }
+    const rawPages =
+      getDatedReleaseNotesRawPages(contentCatalog)
+    const pageUiModels = turnRawPagesIntoPageUiModels(
+      site,
+      rawPages,
+      contentCatalog
     )
-    const { buildPageUiModel } = module.parent.require(
-      '@antora/page-composer/build-ui-model'
-    )
-    const pagesUIModel = pages
-      .map((page) =>
-        buildPageUiModel(site, page, contentCatalog)
-      )
-      .filter((page) =>
-        isValidDate(page.attributes?.revdate)
-      )
-      .sort(byDate)
     return getMostRecentlyUpdatedPages(
-      pagesUIModel,
+      pageUiModels,
       numOfItems
     )
   }
 }
 
-function isReleaseNotesWithRevdate (asciidoc) {
+function getDatedReleaseNotesRawPages (contentCatalog) {
+  return contentCatalog.getPages(({ asciidoc, out }) => {
+    if (!asciidoc || !out) return
+    return getReleaseNotesWithRevdate(asciidoc)
+  })
+}
+
+function getReleaseNotesWithRevdate (asciidoc) {
+  const attributes = asciidoc.attributes
   return (
     asciidoc.attributes &&
-    asciidoc.attributes['page-component-name'] ===
-      'release-notes' &&
-    asciidoc.attributes['page-revdate']
+    isReleaseNotes(attributes) &&
+    hasRevDate(attributes)
   )
 }
 
-function byDate (a, b) {
+function isReleaseNotes (attributes) {
   return (
-    new Date(b.attributes.revdate) -
-    new Date(a.attributes.revdate)
+    attributes['page-component-name'] === 'release-notes'
   )
 }
 
-function getMostRecentlyUpdatedPages (
-  pagesUIModel,
-  numOfItems
+function hasRevDate (attributes) {
+  return attributes['page-revdate']
+}
+
+function turnRawPagesIntoPageUiModels (
+  site,
+  pages,
+  contentCatalog
 ) {
-  const resultList = []
-  const maxNumberOfPages =
-    pagesUIModel.length > numOfItems
-      ? numOfItems
-      : pagesUIModel.length
-  for (let i = 0; i < maxNumberOfPages; i++) {
-    const page = pagesUIModel[i]
-    if (page.attributes?.revdate) {
-      resultList.push({
-        revdateWithoutYear: removeYear(
-          page.attributes?.revdate
-        ),
-        title: page.title,
-        url: page.url,
-      })
-    }
-  }
-  return resultList
+  const { buildPageUiModel } = module.parent.require(
+    '@antora/page-composer/build-ui-model'
+  )
+  return pages
+    .map((page) =>
+      buildPageUiModel(site, page, contentCatalog)
+    )
+    .filter((page) => isValidDate(page.attributes?.revdate))
+    .sort(sortByRevDate)
 }
 
 function isValidDate (dateStr) {
@@ -70,9 +62,78 @@ function isValidDate (dateStr) {
   return !isNaN(dateObj)
 }
 
+function sortByRevDate (a, b) {
+  return (
+    new Date(b.attributes.revdate) -
+    new Date(a.attributes.revdate)
+  )
+}
+
+function getMostRecentlyUpdatedPages (
+  pageUIModels,
+  numOfItems
+) {
+  const maxNumberOfPages =
+    pageUIModels.length > numOfItems
+      ? numOfItems
+      : pageUIModels.length
+  const resultList = getResultList(
+    pageUIModels,
+    maxNumberOfPages
+  )
+  return resultList
+}
+
+function getResultList (pageUIModels, maxNumberOfPages) {
+  const resultList = []
+  for (let i = 0; i < maxNumberOfPages; i++) {
+    const page = pageUIModels[i]
+    if (page.attributes?.revdate) {
+      resultList.push(getSelectedAttributes(page))
+    }
+  }
+  return resultList
+}
+
+function getSelectedAttributes (page) {
+  const latestVersion = getLatestVersion(
+    page.contents.toString()
+  )
+  return {
+    latestVersionAnchor: latestVersion?.anchor,
+    latestVersionName: latestVersion?.innerText,
+    revdateWithoutYear: removeYear(
+      page.attributes?.revdate
+    ),
+    title: cleanTitle(page.title),
+    url: page.url,
+  }
+}
+
+function getLatestVersion (contentsStr) {
+  const nodes = parseHTML(contentsStr)
+  const firstVersion = nodes.querySelector('h2')
+  if (firstVersion) {
+    return {
+      anchor: firstVersion.id,
+      innerText: firstVersion.innerText,
+    }
+  }
+}
+
+function parseHTML (html) {
+  const { parse } = require('node-html-parser')
+  return parse(html)
+}
+
 function removeYear (dateStr) {
   const dateObj = new Date(dateStr)
   return `${dateObj.toLocaleString('default', {
     month: 'short',
   })} ${dateObj.getDate()}`
+}
+
+function cleanTitle (title) {
+  const splitList = title.split('Release Notes')
+  return splitList[0].trim()
 }
