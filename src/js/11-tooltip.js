f0e79f56f42c779c38164a6a32c1f4901c9688e1
Gary Cheung <gcheung@mulesoft.com>
2022-11-09 16:16:30 -0800

    add alt texts

--- src/js/11-tooltip.js
+++ src/js/11-tooltip.js
@@ -2,40 +2,49 @@
   'use strict'
 
   const archiveLinks = document.querySelectorAll("a[href='https://archive.docs.mulesoft.com/']")
-  var uiRootPath = document.getElementById('site-script').dataset.uiRootPath
   archiveLinks.forEach(function (archiveLink) {
-    const tooltipDiv = document.createElement('div')
-    tooltipDiv.classList.add('tooltip-div')
+    const tooltipIcon = createTooltipIcon(archiveLink)
+    const tooltipDiv = createTooltipDiv()
+    tooltipDiv.appendChild(tooltipIcon)
     archiveLink.parentElement.appendChild(tooltipDiv)
-    const colorScheme = inLeftNav(archiveLink) ? 'gray' : 'white'
-    const toolTip = createToolTip(colorScheme)
-    tooltipDiv.appendChild(toolTip)
-
-    tippy(toolTip, {
-      arrow: tippy.roundArrow,
-      content:
-        'When a product version is no longer supported, ' +
-        'including products with end-of-life status, ' +
-        'its documentation moves to an archive site.',
-      duration: [0, 150],
-      maxWidth: 240,
-      offset: [0, 15],
-      theme: `${colorScheme}-archive-link-popover`,
-      touchHold: true, // maps touch as click (for some reason)
-      zIndex: 16, // same as z-nav-mobile
-    })
   })
+
+  function createTooltipIcon (archiveLink) {
+    const color = inLeftNav(archiveLink) ? 'gray' : 'white'
+    const tooltipIcon = document.createElement('img')
+    tooltipIcon.classList.add('tooltip')
+    tooltipIcon.setAttribute('role', 'tool-tip')
+    tooltipIcon.setAttribute('tabindex', '0')
+    tooltipIcon.setAttribute('alt', 'Archived Documentation information')
+    const uiRootPath = document.getElementById('site-script').dataset.uiRootPath
+    tooltipIcon.src = `${uiRootPath}/img/icons/tooltip-${color}.svg`
+    addTooltip(tooltipIcon, color)
+    return tooltipIcon
+  }
 
   function inLeftNav (archiveLink) {
     return archiveLink.classList.contains('nav-text')
   }
 
-  function createToolTip (color) {
-    const img = document.createElement('img')
-    img.setAttribute('role', 'tool-tip')
-    img.setAttribute('tabindex', '0')
-    img.classList.add('tooltip')
-    img.src = `${uiRootPath}/img/icons/tooltip-${color}.svg`
-    return img
+  function addTooltip (tooltipIcon, color) {
+    tippy(tooltipIcon, {
+      arrow: tippy.roundArrow,
+      content:
+      'When a product version is no longer supported, ' +
+      'including products with end-of-life status, ' +
+      'its documentation moves to an archive site.',
+      duration: [0, 150],
+      maxWidth: 240,
+      offset: [0, 15],
+      theme: `${color}-archive-link-popover`,
+      touchHold: true, // maps touch as click (for some reason)
+      zIndex: 16, // same as z-nav-mobile
+    })
+  }
+
+  function createTooltipDiv () {
+    const tooltipDiv = document.createElement('div')
+    tooltipDiv.classList.add('tooltip-div')
+    return tooltipDiv
   }
 })()
