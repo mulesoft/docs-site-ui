feb8cc7130638a02408892e92304e07b5ebc8c9f
Gary Cheung <gcheung@mulesoft.com>
2022-11-16 14:39:41 -0800

    exclude beta sites

--- src/js/01-nav.js
+++ src/js/01-nav.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-var */
-;(function () {
+;(() => {
   'use strict'
 
   function buildNav (navData, nav, page) {
@@ -133,7 +133,7 @@
         groupsAccum.pop()
       } else if (groupComponents.length === 1 && (component = groupComponents[0]).unversioned) {
         var items = component.nav.items
-        if ((items[0] || {}).url === data.homeUrl) {
+        if (!isBetaSite() && ((items[0] || {}).url === data.homeUrl)) {
           component.nav.items = items.slice(1)
         }
         component.nav.items.forEach(function (it) {
