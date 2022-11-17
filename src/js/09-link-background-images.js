35deb9523af4b8a1f4bd9d13271956a89e954855
Gary Cheung <gcheung@mulesoft.com>
2022-11-16 16:22:07 -0800

    refactor

--- src/js/09-link-background-images.js
+++ src/js/09-link-background-images.js
@@ -1,17 +1,13 @@
 ;(() => {
   'use strict'
 
-  const uiRootPath = document.getElementById('site-script').dataset.uiRootPath
+  const processExternalLinks = (selector) => {
+    document.querySelectorAll(`${selector} [target="_blank"]`).forEach((externalLink) => {
+      addLinkImage(externalLink)
+    })
+  }
 
-  document.querySelectorAll('.nav [target="_blank"]').forEach(function (externalLink) {
-    addLinkImage(externalLink)
-  })
-
-  document.querySelectorAll('.doc [target="_blank"]').forEach(function (externalLink) {
-    addLinkImage(externalLink)
-  })
-
-  function addLinkImage (link) {
+  const addLinkImage = (link) => {
     if (!isDataWeavePlaygroundLink(link) && !isFooterLink(link)) {
       const externalLinkImg = createLinkImage('external-link')
       externalLinkImg.alt = 'Leaving the Site'
@@ -20,28 +16,16 @@
     }
   }
 
-  const anchors = document.querySelectorAll('.anchor')
-  anchors.forEach(function (anchor) {
-    const anchorImg = createLinkImage('anchor')
-    const headerText = anchor.parentElement.textContent
-    if (headerText) anchorImg.alt = `Jump to ${headerText}`
-    anchorImg.setAttribute('title', `Jump to ${headerText}`)
+  const isDataWeavePlaygroundLink = (e) => {
+    return e.classList.contains('dw-playground-link')
+  }
 
-    anchor.addEventListener('click', function () {
-      adjustScrollPosition(anchor)
-    })
-    anchor.appendChild(anchorImg)
+  const isFooterLink = (e) => {
+    return e.offsetParent.tagName === 'FOOTER'
+  }
 
-    const sideLinks = [...document.querySelectorAll('.toc-menu a')]
-    const sideLink = sideLinks.filter((a) => a.textContent === headerText)
-    if (sideLink.length > 0) {
-      sideLink[0].addEventListener('click', function () {
-        adjustScrollPosition(anchor)
-      })
-    }
-  })
-
-  function createLinkImage (element) {
+  const createLinkImage = (element) => {
+    const uiRootPath = document.getElementById('site-script').dataset.uiRootPath
     const img = document.createElement('img')
     img.setAttribute('role', 'link')
     img.classList.add(`${element}-image`)
@@ -49,18 +33,42 @@
     return img
   }
 
-  function isDataWeavePlaygroundLink (e) {
-    return e.classList.contains('dw-playground-link')
+  const processAnchorLinks = () => {
+    document.querySelectorAll('.anchor').forEach((anchor) => {
+      const anchorImg = createLinkImage('anchor')
+      const headerText = anchor.parentElement.textContent
+      if (headerText) anchorImg.alt = `Jump to ${headerText}`
+      anchorImg.setAttribute('title', `Jump to ${headerText}`)
+
+      anchor.addEventListener('click', () => {
+        adjustScrollPosition(anchor)
+      })
+      anchor.appendChild(anchorImg)
+
+      const sideLinks = [...document.querySelectorAll('.toc-menu a')].filter((a) => a.textContent === headerText)
+      if (sideLinks.length > 0) {
+        sideLinks[0].addEventListener('click', () => {
+          adjustScrollPosition(anchor)
+        })
+      }
+    })
   }
 
-  function isFooterLink (e) {
-    return e.offsetParent.tagName === 'FOOTER'
+  const processSamePageLinks = () => {
+    const samePageLinks = [...document.querySelectorAll('.doc a')].filter((a) => a.href.includes('#'))
+    samePageLinks.forEach((samePageLink) => {
+      const href = samePageLink.href.split('#')[1]
+      const destLinkElement = document.querySelector(`#${href}`)
+      samePageLink.addEventListener('click', () => {
+        adjustScrollPosition(destLinkElement)
+      })
+    })
   }
 
-  function adjustScrollPosition (anchor) {
+  const adjustScrollPosition = (anchor) => {
     const minHeight = getMinHeight()
     var tries = 0
-    var autoScrollDown = setInterval(function () {
+    var autoScrollDown = setInterval(() => {
       if (anchor.getBoundingClientRect().top <= minHeight) {
         window.scrollBy(0, -minHeight / 1.1)
         clearInterval(autoScrollDown)
@@ -71,9 +79,14 @@
     }, 50)
   }
 
-  function getMinHeight () {
+  const getMinHeight = () => {
     const toolbar = document.querySelector('.toolbar')
     const noticeBanner = document.querySelector('.notice-banner')
     return noticeBanner ? toolbar.scrollHeight + noticeBanner.scrollHeight : toolbar.scrollHeight
   }
+
+  processExternalLinks('.nav')
+  processExternalLinks('.doc')
+  processAnchorLinks()
+  processSamePageLinks()
 })()
