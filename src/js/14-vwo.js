8a6eff2de6c5a4dc1a31cc44d344ff41385f568b
Gary Cheung <gcheung@mulesoft.com>
2022-12-08 16:25:07 -0800

    add search page

--- src/js/14-vwo.js
+++ src/js/14-vwo.js
@@ -1,7 +1,7 @@
 ;(() => {
   'use strict'
 
-  const landingPagePathnames = ['/general/', '/general/index', '/general/11-2022-index']
+  const landingPagePathnames = ['/', '/general/', '/general/index', '/general/11-2022-index']
 
   const isLandingPage = () => {
     const output = landingPagePathnames.filter((pathName) => window.location.pathname.endsWith(pathName))
@@ -16,7 +16,7 @@
         window._vis_opt_queue.push(() => {
           try {
             /* eslint-disable no-undef */
-            _vis_opt_register_conversion(2145)
+            _vis_opt_register_conversion(2, 145)
             /* eslint-enable no-undef */
           } catch (_error) {
             console.warn('_vis_opt_register_conversion not defined. Skipping...')
