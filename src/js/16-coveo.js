0de7165cde5cbd830eb426233a67b7da11f1db55
Gary Cheung <gcheung@mulesoft.com>
2022-12-12 14:55:24 -0800

    refactor

--- src/js/16-coveo.js
+++ src/js/16-coveo.js
@@ -1,16 +1,19 @@
 ;(() => {
   'use strict'
 
-  let tries = 10
-
   const updateAtomicElements = () => {
+    let tries = 10
     const setPlaceholderText = setInterval(() => {
-      const atomicSearchBox = document.querySelector('atomic-search-box')
-      const root = atomicSearchBox && atomicSearchBox.shadowRoot
-      if (root) {
-        const comboBox = root.querySelector('[role="combobox"]')
+      const atomicSearchbox = document.querySelector('atomic-search-box')
+      const searchboxShadowRoot = atomicSearchbox && atomicSearchbox.shadowRoot
+      if (searchboxShadowRoot) {
+        const comboBox = searchboxShadowRoot.querySelector('[role="combobox"]')
         if (comboBox) {
           comboBox.placeholder = 'Search Docs'
+          const searchboxDiv = searchboxShadowRoot.querySelector('div')
+          if (searchboxDiv) {
+            searchboxDiv.style.maxWidth = '80%'
+          }
           clearInterval(setPlaceholderText)
         }
       }
