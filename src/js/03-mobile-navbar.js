4bc27351df210acf0e04aec7785cd8853a907d17
Gary Cheung <gcheung@mulesoft.com>
2023-01-20 13:13:00 -0800

    refactor

--- src/js/03-mobile-navbar.js
+++ src/js/03-mobile-navbar.js
@@ -1,35 +1,80 @@
-;(function () {
+;(() => {
   'use strict'
 
-  function bindNavToggle (backdrop, body, nav, navToggle, navCloseButton) {
-    if (backdrop) {
-      backdrop.addEventListener('click', toggleNav.bind(nav, backdrop, body))
+  class MobileNav {
+    constructor (backdrop, body, nav, navToggle, navCloseButton) {
+      this.backdrop = backdrop
+      this.body = body
+      this.nav = nav
+      this.navToggle = navToggle
+      this.navCloseButton = navCloseButton
+
+      this.tabindexStoreMap = {}
     }
-    if (navToggle) {
-      navToggle.addEventListener('click', toggleNav.bind(nav, backdrop, body))
+
+    addNavToggleListeners () {
+      if (this.backdrop) {
+        this.backdrop.addEventListener('click', (e) => this.toggleNav(e))
+      }
+      if (this.navToggle) {
+        this.navToggle.addEventListener('click', (e) => this.toggleNav(e))
+      }
+      if (this.navCloseButton) {
+        this.navCloseButton.addEventListener('click', (e) => this.toggleNav(e))
+      }
     }
-    if (navCloseButton) {
-      navCloseButton.addEventListener('click', toggleNav.bind(nav, backdrop, body))
+
+    toggleNav (e) {
+      if (e.target === this.backdrop && !this.navIsActive()) {
+        return
+      }
+      this.body.classList.toggle('mobile')
+      this.body.classList.toggle('no-scroll')
+      this.backdrop.classList.toggle('mobile')
+      this.backdrop.classList.toggle('show')
+      this.nav.classList.toggle('is-active')
+      this.navCloseButton.classList.toggle('hide')
+      this.toggleTabIndexOutsideNav()
+      e.stopPropagation()
+    }
+
+    toggleTabIndexOutsideNav () {
+      const links = document.querySelectorAll('a, button, .tooltip')
+      links.forEach((link) => {
+        if (!this.inLeftnav(link)) {
+          const tabIndex = link.tabIndex
+          link.removeAttribute('tabindex')
+          const linkPath = link.outerHTML
+          if (linkPath in this.tabindexStoreMap && this.tabindexStoreMap[linkPath] != null) {
+            link.tabIndex = this.tabindexStoreMap[linkPath]
+            this.tabindexStoreMap[linkPath] = null
+          } else {
+            this.tabindexStoreMap[linkPath] = tabIndex
+            link.tabIndex = -1
+          }
+        }
+      })
+      if (this.navIsActive()) {
+        document.querySelector('#search-button').focus()
+      }
+    }
+
+    inLeftnav (element) {
+      return this.nav.contains(element)
+    }
+
+    navIsActive () {
+      return this.nav.classList.contains('is-active')
     }
   }
 
-  function toggleNav (backdrop, body, e) {
-    if (e.target === backdrop && !this.classList.contains('is-active')) {
-      return
-    }
-    body.classList.toggle('mobile')
-    body.classList.toggle('no-scroll')
-    backdrop.classList.toggle('mobile')
-    backdrop.classList.toggle('show')
-    this.classList.toggle('is-active')
-    e.stopPropagation()
-  }
-
-  bindNavToggle(
+  const mobileNav = new MobileNav(
     document.querySelector('.modal-backdrop'),
     document.body,
-    document.querySelector('.nav'),
+    document.querySelector('.nav.fit'),
     document.querySelector('.nav-toggle'),
     document.querySelector('.nav-close-button')
   )
+
+  mobileNav.addNavToggleListeners()
 })()
