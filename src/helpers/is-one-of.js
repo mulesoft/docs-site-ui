72dcb6fecb190e67ff37df8691f8f1d2e51c8596
Gary Cheung <gcheung@mulesoft.com>
2023-03-15 08:34:18 -0700

    add codedocs

--- src/helpers/is-one-of.js
+++ src/helpers/is-one-of.js
@@ -1,10 +1,21 @@
 'use strict'
 
+/**
+ * Given a list of arguments, check whether the first argument is the same as 1 of the other arguments.
+ *  example 1 - (is-one-of a a b c) -> true
+ *  example 2 - (is-one-of c a b c) -> true
+ *  example 3 - (is-one-of d a b c) -> false
+ *  example 4 - (is-one-of c) -> false
+ *  example 5 - (is-one-of) -> false
+ * @returns true if the first argument matches any of the subsequent arguments, false otherwise.
+ */
+
 module.exports = function () {
-  if (arguments.length === 0) return false
+  if (arguments.length <= 1) return false
   const values = Object.values(arguments)
   const baseValue = values[0]
-  for (let i = 1; i < values.length - 1; i++) {
+  const loopLength = values.length - 1 // the last item of the values array is a self-reference, so exclude it
+  for (let i = 1; i < loopLength; i++) {
     if (baseValue === values[i]) {
       return true
     }
--- src/helpers/or.js
+++ src/helpers/or.js
@@ -1,8 +1,21 @@
 'use strict'
 
+/**
+ * Given a list of arguments, loop through the list and return the argument is not false, undefined, or null.
+ *  example 1 - (or true-statement false-statement) -> true-statement
+ *  example 2 - (or false-statement false-statement-2 true-statement false-statement-3) -> true-statement
+ *  example 3 - (or true-statement) -> true
+ *  example 4 - (or false-statement) -> false
+ *  example 5 - (or false-statement false-statement-2 false-statement-3 false-statement-4) -> false
+ *  example 6 - (or) -> false
+ * @returns the first argument from the list that is not false, undefined, or null. False if no such argument exists
+ */
+
 module.exports = function () {
+  if (arguments.length <= 1) return false
   const values = Object.values(arguments)
-  for (let i = 0; i < values.length - 1; i++) {
+  const loopLength = values.length - 1 // the last item of the values array is a self-reference, so exclude it
+  for (let i = 0; i < loopLength; i++) {
     const value = values[i]
     if (value !== false && value !== undefined && value !== null) {
       return value
