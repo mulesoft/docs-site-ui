92e5e932d2fb9a67471a9bc4a833e2819d6efd84
Gary Cheung <gcheung@mulesoft.com>
2023-03-15 07:58:09 -0700

    make or better

--- src/helpers/or.js
+++ src/helpers/or.js
@@ -1,3 +1,12 @@
 'use strict'
 
-module.exports = (a, b) => a || b
+module.exports = function () {
+  const values = Object.values(arguments)
+  for (let i = 0; i < values.length - 1; i++) {
+    const value = values[i]
+    if (value !== false && value !== undefined && value !== null) {
+      return value
+    }
+  }
+  return false
+}
