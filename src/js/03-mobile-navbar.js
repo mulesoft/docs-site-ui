f0c4fe03f209f6b4ebe50986c9d30a1f3d652001
Gary Cheung <gcheung@mulesoft.com>
2023-04-21 16:50:17 -0700

    refactor

--- src/js/03-mobile-navbar.js
+++ src/js/03-mobile-navbar.js
@@ -1,192 +1,127 @@
 ;(() => {
   'use strict'
 
-  class MobileNav {
-    constructor () {
-      this.backdrop = document.querySelector('.modal-backdrop')
-      this.body = document.body
-      this.nav = document.querySelector('.nav.fit')
-      this.toolbarMenuButton = document.querySelector('.nav-toggle')
-      this.navCloseButton = document.querySelector('.nav-close-button')
-      this.toolbarSearchButton = document.querySelector('.toolbar-search-button')
-      this.leftNavSkipLink = this.getLeftNavSkipLink()
-      this.mobileNavFocusTrapper = document.querySelector('.mobile-nav-focus-trapper')
-      this.tabindexStoreMap = {}
-    }
+  const backdrop = document.querySelector('.modal-backdrop')
+  const body = document.body
+  const nav = document.querySelector('.nav.fit')
+  const toolbarMenuButton = document.querySelector('.nav-toggle')
+  const navCloseButton = document.querySelector('.nav-close-button')
+  const toolbarSearchButton = document.querySelector('.toolbar-search-button')
+  const skipLinks = document.querySelectorAll('.skip-link')
+  const mobileNavFocusTrapper = document.querySelector('.mobile-nav-focus-trapper')
+  const tabindexStoreMap = {}
 
-    addNavToggleListeners () {
-      if (this.backdrop) {
-        this.backdrop.addEventListener('click', (e) => this.hideNav(e))
-      }
-      if (this.toolbarMenuButton) {
-        this.toolbarMenuButton.addEventListener('click', (e) => this.toggleNav(e))
-      }
-      if (this.navCloseButton) {
-        this.navCloseButton.addEventListener('click', (e) => this.toggleNav(e))
-      }
-      if (this.toolbarSearchButton) {
-        this.toolbarSearchButton.addEventListener('click', (e) => {
-          this.toggleNav(e)
-          this.focusOnMobileNavSearchBox()
-        })
-      }
-      if (this.leftNavSkipLink) {
-        // this takes precedence than the normal skip link listener in 15-skiplink-listeners.js
-        this.leftNavSkipLink.addEventListener('click', (e) => {
-          if (!isBigScreenSize()) this.toggleNav(e)
-        })
-      }
-      document.addEventListener('keydown', (e) => {
-        if (e.keyCode === 27) {
-          if (this.mobileNavIsActive()) {
-            this.hideNav(e)
-          } else {
-            this.toggleTabIndexOutsideNav()
-          }
-          this.toggleFocus()
-        }
-      })
-    }
-
-    hideNav (e) {
-      this.toggleNav(e, false)
-    }
-
-    toggleFocus () {
-      if (this.mobileNavFocusTrapper) {
-        if (this.mobileNavIsActive()) {
-          this.mobileNavFocusTrapper.tabIndex = 0
-          this.mobileNavFocusTrapper.focus()
-        } else {
-          this.mobileNavFocusTrapper.tabIndex = -1
-          this.toolbarMenuButton.focus()
-        }
+  const addListeners = (backdrop, toolbarMenuButton, navCloseButton, toolbarSearchButton, skipLinks) => {
+    const handleEscapeKeydown = (e) => {
+      if (e.key === 'Escape') {
+        mobileNavIsActive()
+          ? hideNav(e)
+          : toggleTabIndexOutsideNav()
+        toggleFocus()
       }
     }
 
-    focusOnMobileNavSearchBox () {
-      if (this.mobileNavIsActive()) {
-        const searchBox = this.getFocusableSearchBox()
-        if (searchBox) {
-          searchBox.focus()
-        } else {
-          this.toggleFocus()
-        }
+    const getLeftNav = (skipLinks) => {
+      for (const skipLink of skipLinks) {
+        if (skipLink.innerHTML.includes('left navigation')) return skipLink
       }
     }
 
-    getFocusableSearchBox () {
+    const handleMobileLeftNavSkipLinkClick = (e) => { if (!isBigScreenSize()) toggleNav(e) }
+
+    const handleToolbarSearchButtonClick = (e) => {
+      toggleNav(e)
+      focusOnMobileNavSearchBox()
+    }
+
+    if (backdrop) backdrop.addEventListener('click', hideNav)
+    if (toolbarMenuButton) toolbarMenuButton.addEventListener('click', toggleNav)
+    if (navCloseButton) navCloseButton.addEventListener('click', toggleNav)
+    if (toolbarSearchButton) toolbarSearchButton.addEventListener('click', handleToolbarSearchButtonClick)
+    // this takes precedence than the normal skip link listener in 15-skiplink-listeners.js
+    const leftNavSkipLink = getLeftNav(skipLinks)
+    if (leftNavSkipLink) leftNavSkipLink.addEventListener('click', handleMobileLeftNavSkipLinkClick)
+    document.addEventListener('keydown', handleEscapeKeydown)
+  }
+
+  const focusOnMobileNavSearchBox = () => {
+    const getFocusableSearchBox = () => {
       const atomicSearchbox = document.querySelector('atomic-search-box')
-      const searchboxShadowRoot = atomicSearchbox && atomicSearchbox.shadowRoot
-      if (searchboxShadowRoot) {
-        return searchboxShadowRoot.querySelector('input')
-      }
+      const searchboxShadowRoot = atomicSearchbox?.shadowRoot
+      return searchboxShadowRoot?.querySelector('input')
     }
 
-    getLeftNavSkipLink () {
-      let output
-      const skipLinks = document.querySelectorAll('.skip-link')
-      skipLinks.forEach((skipLinks) => {
-        if (skipLinks.innerHTML.includes('left navigation')) output = skipLinks
-      })
-      return output
-    }
-
-    inLeftnav (element) {
-      return this.nav.contains(element)
-    }
-
-    mobileNavIsActive () {
-      return this.nav.classList.contains('is-active')
-    }
-
-    setTabindex (link) {
-      if (!this.inLeftnav(link)) {
-        const tabIndex = link.tabIndex
-        const linkPath = getXPath(link)
-        link.removeAttribute('tabindex')
-        if (this.mobileNavIsActive()) {
-          if (!(linkPath in this.tabindexStoreMap) || this.tabindexStoreMap[linkPath] == null) {
-            this.tabindexStoreMap[linkPath] = tabIndex
-          }
-          link.tabIndex = -1
-        } else {
-          if (linkPath in this.tabindexStoreMap && this.tabindexStoreMap[linkPath] != null) {
-            link.tabIndex = this.tabindexStoreMap[linkPath]
-            this.tabindexStoreMap[linkPath] = null
-          }
-        }
-      }
-    }
-
-    toggleNav (e, override) {
-      switch (override) {
-        case true:
-          this.nav.classList.add('is-active')
-          break
-        case false:
-          this.nav.classList.remove('is-active')
-          break
-        default:
-          this.nav.classList.toggle('is-active')
-          break
-      }
-
-      if (this.mobileNavIsActive()) {
-        this.body.classList.add('mobile')
-        this.body.classList.add('no-scroll')
-        this.backdrop.classList.add('mobile')
-        this.backdrop.classList.add('show')
-        this.navCloseButton.classList.remove('hide')
-      } else {
-        this.body.classList.remove('mobile')
-        this.body.classList.remove('no-scroll')
-        this.backdrop.classList.remove('mobile')
-        this.backdrop.classList.remove('show')
-        this.navCloseButton.classList.add('hide')
-      }
-
-      this.toggleTabIndexOutsideNav()
-      this.toggleFocus()
-      e.stopPropagation()
-    }
-
-    toggleTabIndexOutsideNav () {
-      const links = document.querySelectorAll('a, button, select, .tooltip, .menu-ham')
-      links.forEach((link) => {
-        this.setTabindex(link)
-      })
+    if (mobileNavIsActive()) {
+      const searchBox = getFocusableSearchBox()
+      searchBox
+        ? searchBox.focus()
+        : toggleFocus()
     }
   }
+
+  const hideNav = (e) => toggleNav(e, false)
+  const inLeftnav = (element) => nav.contains(element)
+  const isBigScreenSize = () => window.innerWidth >= 768
 
   // source:
   // https://stackoverflow.com/questions/2631820/
   // how-do-i-ensure-saved-click-coordinates-can-be-reload-to-the-same-place-even-if/2631931
   const getXPath = (element) => {
-    if (element.id !== '') {
-      return 'id("' + element.id + '")'
-    }
-
-    if (element === document.body) {
-      return element.tagName
-    }
+    if (element.id !== '') return `id("${element.id}")`
+    if (element === document.body) return element.nodeName.toLowerCase()
 
     let ix = 0
     const siblings = element.parentNode.childNodes
     for (const sibling of siblings) {
-      if (sibling === element) {
-        return getXPath(element.parentNode) + '/' + element.tagName + '[' + (ix + 1) + ']'
-      }
-      if (sibling.nodeType === 1 && sibling.tagName === element.tagName) {
-        ix++
+      if (sibling === element) return `${getXPath(element.parentNode)}/${element.tagName}[${ix + 1}]`
+      if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++
+    }
+  }
+
+  const mobileNavIsActive = () => nav.classList.contains('is-active')
+
+  const setTabindex = (link) => {
+    if (!inLeftnav(link)) {
+      const tabIndex = link.tabIndex
+      const linkPath = getXPath(link)
+      link.removeAttribute('tabindex')
+      if (mobileNavIsActive()) {
+        if (!(linkPath in tabindexStoreMap) || tabindexStoreMap[linkPath] == null) {
+          tabindexStoreMap[linkPath] = tabIndex
+        }
+        link.tabIndex = -1
+      } else if (linkPath in tabindexStoreMap && tabindexStoreMap[linkPath] != null) {
+        link.tabIndex = tabindexStoreMap[linkPath]
+        tabindexStoreMap[linkPath] = null
       }
     }
   }
 
-  const isBigScreenSize = () => {
-    return window.innerWidth >= 768
+  const toggleFocus = () => {
+    const isActive = mobileNavIsActive()
+    mobileNavFocusTrapper.tabIndex = isActive ? 0 : -1
+    isActive ? mobileNavFocusTrapper?.focus() : toolbarMenuButton.focus()
   }
 
-  const mobileNav = new MobileNav()
-  mobileNav.addNavToggleListeners()
+  const toggleNav = (e, override) => {
+    let navOperation = 'toggle'
+    if (override !== undefined) navOperation = override ? 'add' : 'remove'
+    nav.classList[navOperation]('is-active')
+
+    const miscOperation = mobileNavIsActive() ? 'add' : 'remove'
+    body.classList[miscOperation]('mobile', 'no-scroll')
+    backdrop.classList[miscOperation]('mobile', 'show')
+    navCloseButton.classList[mobileNavIsActive() ? 'remove' : 'add']('hide')
+
+    toggleTabIndexOutsideNav()
+    toggleFocus()
+    e.stopPropagation()
+  }
+
+  const toggleTabIndexOutsideNav = () => {
+    const links = document.querySelectorAll('a, button, select, .tooltip, .menu-ham')
+    links.forEach((link) => setTabindex(link))
+  }
+
+  addListeners(backdrop, toolbarMenuButton, navCloseButton, toolbarSearchButton, skipLinks)
 })()
