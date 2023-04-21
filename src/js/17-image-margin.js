477f9068d02838fd0bbf84eea29fb8d884850039
Gary Cheung <gcheung@mulesoft.com>
2023-04-21 08:56:03 -0700

    increase reliability

--- src/js/17-image-margin.js
+++ src/js/17-image-margin.js
@@ -1,19 +1,15 @@
 ;(() => {
   'use strict'
 
-  // add extra spaces to the top and bottom of big images on the page
+  const isBig = (image) => image.height > 40
+  const isHomePage = () => ['/', '/general/'].includes(window.location.pathname)
 
-  const addMargins = (image) => {
-    image.style.marginTop = '24px'
-    image.style.marginBottom = '8px'
+  if (!isHomePage()) {
+    const pageImages = document.querySelectorAll('.doc img')
+    for (const image of pageImages) {
+      if (isBig(image)) {
+        image.classList.add('page-images')
+      }
+    }
   }
-
-  const isBig = (image) => {
-    return image.height > 20
-  }
-
-  const pageImages = document.querySelectorAll('.doc img')
-  pageImages.forEach((image) => {
-    if (isBig(image)) addMargins(image)
-  })
 })()
