aa6ff3bc6ffae3f5b26547884be723db8979279e
Gary Cheung <gcheung@mulesoft.com>
2023-04-27 17:51:26 -0700

    optimize script

--- src/js/20-mobile-survey.js
+++ src/js/20-mobile-survey.js
@@ -1,17 +1,20 @@
 ;(async () => {
   'use strict'
 
-  const hasAside = () => {
-    const aside = document.querySelector('aside')
-    return (
-      aside &&
-      window.getComputedStyle(aside).display !== 'none' &&
-      window.getComputedStyle(aside).visibility !== 'hidden'
-    )
-  }
+  const aside = document.querySelector('aside')
+  const hideClass = 'hide'
 
-  const hide = (element) => element.classList.add('hide')
-  const show = (element) => element.classList.remove('hide')
+  const isExpanded = (element) => element.ariaExpanded !== 'false'
+
+  const isVisible = (element) => element &&
+    window.getComputedStyle(element).display !== 'none' &&
+    window.getComputedStyle(element).visibility !== 'hidden'
+
+  const hide = (element) => element && element.classList.add(hideClass)
+  const show = (element) => element && element.classList.remove(hideClass)
+
+  const toggleAriaExpanded = (element, bool) => element && element.setAttribute('aria-expanded', bool)
+  const toggleVisibility = (element, bool) => element && element.classList.toggle(hideClass, bool)
 
   // For some reason, mobile survey doesn't show up right after the page loads until I add this timeout.
   // Keep this timeout here for now until we have a better solution
@@ -22,33 +25,28 @@
     const mobileSurveyButton = mobileSurveyDiv.querySelector('button.mobile-survey-button')
     const mobileSurveyHelpText = mobileSurveyDiv.querySelector('p.mobile-survey-help-text')
     const mobileSurveySection = mobileSurveyDiv.querySelector('section.mobile-survey')
+
     const mobileSurveyIconImage = mobileSurveyButton.querySelector('img.survey-icon-image')
     const mobileSurveyIconCloseImage = mobileSurveyButton.querySelector('img.survey-icon-close-image')
-    console.log(mobileSurveyIconImage)
 
-    const toggleVisibility = () => {
-      hasAside() ? mobileSurveyDiv.classList.add('hide') : mobileSurveyDiv.classList.remove('hide')
-    }
-
-    window.addEventListener('resize', toggleVisibility)
-    toggleVisibility()
+    window.addEventListener('resize', () => toggleVisibility(mobileSurveyDiv, isVisible(aside)))
+    toggleVisibility(mobileSurveyDiv, isVisible(aside))
 
     if (mobileSurveyButton) {
-      mobileSurveyButton.addEventListener('click', (e) => {
-        const mobileSurveyIsExpanded = mobileSurveyButton.ariaExpanded !== 'false'
-        const operation = mobileSurveyIsExpanded ? 'add' : 'remove'
-
-        // TODO: add mobile behavior
-        if (mobileSurveySection) mobileSurveySection.classList[operation]('hide')
-        if (mobileSurveyButton) mobileSurveyButton.setAttribute('aria-expanded', !mobileSurveyIsExpanded)
-
-        if (mobileSurveyIconImage) mobileSurveyIconImage.classList.toggle('hide', !mobileSurveyIsExpanded)
-        if (mobileSurveyIconCloseImage) mobileSurveyIconCloseImage.classList[operation]('hide')
-        e.preventDefault()
-      })
-
       mobileSurveyButton.addEventListener('mouseenter', () => show(mobileSurveyHelpText))
       mobileSurveyButton.addEventListener('mouseleave', () => hide(mobileSurveyHelpText))
+
+      mobileSurveyButton.addEventListener('click', (e) => {
+        const mobileSurveyIsExpanded = isExpanded(mobileSurveyButton)
+
+        // TODO: add mobile behavior
+        toggleVisibility(mobileSurveySection, mobileSurveyIsExpanded)
+        toggleVisibility(mobileSurveyIconImage, !mobileSurveyIsExpanded)
+        toggleVisibility(mobileSurveyIconCloseImage, mobileSurveyIsExpanded)
+
+        toggleAriaExpanded(mobileSurveyButton, !mobileSurveyIsExpanded)
+        e.preventDefault()
+      })
     }
   }, 50)
 })()
