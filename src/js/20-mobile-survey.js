2bfd82dda34f989abf0f318338da6311cda473d9
Gary Cheung <gcheung@mulesoft.com>
2023-05-01 15:25:29 -0700

    implement tabindex changes for mobile

--- src/js/20-mobile-survey.js
+++ src/js/20-mobile-survey.js
@@ -4,6 +4,24 @@
   const aside = document.querySelector('aside')
   const backdrop = document.querySelector('.modal-backdrop')
   const hideClass = 'hide'
+  const tabindexStoreMap = {}
+
+  const contains = (parentElement, element) => parentElement.contains(element)
+
+  // source:
+  // https://stackoverflow.com/questions/2631820/
+  // how-do-i-ensure-saved-click-coordinates-can-be-reload-to-the-same-place-even-if/2631931
+  const getXPath = (element) => {
+    if (element.id !== '') return `id("${element.id}")`
+    if (element === document.body) return element.nodeName.toLowerCase()
+
+    let ix = 0
+    const siblings = element.parentNode.childNodes
+    for (const sibling of siblings) {
+      if (sibling === element) return `${getXPath(element.parentNode)}/${element.tagName}[${ix + 1}]`
+      if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++
+    }
+  }
 
   const isExpanded = (element) => element.ariaExpanded !== 'false'
   const isMobileScreen = () => !window.matchMedia(' (min-width: 768px)').matches
@@ -13,9 +31,31 @@
     window.getComputedStyle(element).display !== 'none' &&
     window.getComputedStyle(element).visibility !== 'hidden'
 
+  const setTabindex = (parentElement, link, yes) => {
+    if (!contains(parentElement, link)) {
+      const tabIndex = link.tabIndex
+      const linkPath = getXPath(link)
+      link.removeAttribute('tabindex')
+      if (yes) {
+        if (!(linkPath in tabindexStoreMap) || tabindexStoreMap[linkPath] == null) {
+          tabindexStoreMap[linkPath] = tabIndex
+        }
+        link.tabIndex = -1
+      } else if (linkPath in tabindexStoreMap && tabindexStoreMap[linkPath] != null) {
+        link.tabIndex = tabindexStoreMap[linkPath]
+        tabindexStoreMap[linkPath] = null
+      }
+    }
+  }
+
   const toggleAttribute = (element, attrName, bool) => element && element.setAttribute(attrName, bool)
   const toggleClass = (element, className, bool) => element && element.classList.toggle(className, bool)
   const toggleHelpText = (helpText, bool) => (helpText.innerHTML = bool ? 'Take Survey' : 'Hide')
+
+  const toggleTabIndexOutsideOf = (element, bool) => {
+    const links = document.querySelectorAll('a, button, select, .tooltip, .menu-ham')
+    links.forEach((link) => setTabindex(element, link, bool))
+  }
 
   // For some reason, mobile survey doesn't show up right after the page loads until I add this timeout.
   // Keep this timeout here for now until we have a better solution
@@ -49,8 +89,15 @@
 
         if (isMobileScreen()) {
           toggleClass(backdrop, 'show', !mobileSurveyIsExpanded)
-          backdrop.addEventListener('click', () => toggleAll(!mobileSurveyIsExpanded))
-          mobileSurveyToggleButton.addEventListener('click', () => toggleAll(!mobileSurveyIsExpanded))
+          backdrop.addEventListener('click', () => {
+            toggleAll(!mobileSurveyIsExpanded)
+            toggleTabIndexOutsideOf(mobileSurveyDiv, false)
+          })
+          mobileSurveyToggleButton.addEventListener('click', () => {
+            toggleAll(!mobileSurveyIsExpanded)
+            toggleTabIndexOutsideOf(mobileSurveyDiv, false)
+          })
+          toggleTabIndexOutsideOf(mobileSurveyDiv, !mobileSurveyIsExpanded)
         }
         toggleAll(mobileSurveyIsExpanded)
         toggleHelpText(mobileSurveyHelpText, mobileSurveyIsExpanded)
