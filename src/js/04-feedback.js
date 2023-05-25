1fcd7e3cb4bea176ad3331d8622e6b95d637adf0
Gary Cheung <gcheung@mulesoft.com>
2023-05-25 13:04:39 -0700

    add button listeners

--- src/js/04-feedback.js
+++ src/js/04-feedback.js
@@ -2,31 +2,45 @@
   'use strict'
 
   const feedbackCard = document.querySelector('section.feedback-section')
-  if (!feedbackCard || !window.analytics) return
+  if (!feedbackCard
+  // TODO: add this back before PR is merged
+  // || !window.analytics
+  ) return
 
-  const buttonLabels = ['Yes', 'No']
+  const feedbackOptionButtons = feedbackCard.querySelectorAll('div.feedback-options button')
+  const feedbackAckMsgDiv = feedbackCard.querySelector('div.feedback-ack')
+  const decision = ['Yes', 'No']
 
-  const addListeners = (feedbackCard, buttonLabels) => {
-    buttonLabels.forEach((msg) => {
-      const feedbackButton = feedbackCard.querySelector(`button.feedback-${msg.toLowerCase()}`)
-      if (feedbackButton) feedbackButton.addEventListener('click', (e) => track(`Clicked Helpful ${msg}`, e))
+  const addListeners = (feedbackCard, decision) => {
+    decision.forEach((decision) => {
+      const feedbackButton = feedbackCard.querySelector(`button.feedback-${decision.toLowerCase()}`)
+      if (feedbackButton) feedbackButton.addEventListener('click', (e) => track(decision, e))
     })
   }
 
-  const flip = (element) => element.classList.add('flip')
+  const hide = (element) => element.classList.add('hide')
+  const show = (element) => element.classList.remove('hide')
 
-  const track = (msg, e) => {
+  const track = (decision, e) => {
     try {
-      window.analytics.track(msg, {
-        title: document.title,
-        url: window.location.href,
-      })
-      flip(feedbackCard)
+      // TODO: add this back before PR is merged
+      // window.analytics.track(`Clicked Helpful ${decision}`, {
+      //   title: document.title,
+      //   url: window.location.href,
+      // })
+      feedbackOptionButtons.forEach((button) => hide(button))
+      show(feedbackAckMsgDiv)
+      updateFeedbackAckMsg(feedbackAckMsgDiv, decision)
     } catch (error) {
       console.warn(error)
     }
     if (e) e.preventDefault()
   }
 
-  addListeners(feedbackCard, buttonLabels)
+  const updateFeedbackAckMsg = (feedbackAckMsgDiv, decision) => {
+    const msg = feedbackAckMsgDiv.querySelector('p')
+    if (msg) msg.innerText += ` ${decision}`
+  }
+
+  addListeners(feedbackCard, decision)
 })()
