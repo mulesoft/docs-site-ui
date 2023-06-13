a6125c10e5a34fca3440f5889fe152416ba5a17e
Gary Cheung <gcheung@mulesoft.com>
2023-06-13 16:59:47 -0700

    [W-12637739] dynamically create version facets (#421)
    * add dynamic version facets
    
    * adjust facet-id
    
    * add a test version
    
    * add allowed-values
    
    * add missing meta tags
    
    * move js files
    
    * revert
    
    * fix ui root path
    
    * remove search field
    
    * remove unneeded facets
    
    * clean up

--- src/js/22-coveo-pager.js
+++ src/js/22-coveo-pager.js
@@ -6,14 +6,12 @@
   let tries = 500
 
   const updateAriaLabel = (paginationNav, partName) => {
-    const button = paginationNav?.querySelector(`button[part="${partName}"]`)
+    const button = paginationNav.querySelector(`button[part="${partName}"]`)
     if (button) button.setAttribute('aria-label', `${button.getAttribute('aria-label')} page`)
   }
 
   const updatePager = setInterval(() => {
-    if (--tries <= 0) {
-      clearInterval(updatePager)
-    }
+    if (--tries <= 0) clearInterval(updatePager)
 
     const atomicPager = document.querySelector('atomic-pager')
     const pagerShadowRoot = atomicPager?.shadowRoot
