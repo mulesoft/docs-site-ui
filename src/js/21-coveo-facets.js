9f808276db6371c19867bf4ce9233077e6b4efe8
Gary Cheung <gcheung@mulesoft.com>
2023-06-13 09:03:41 -0700

    add dynamic version facets

--- src/js/21-coveo-facets.js
+++ src/js/21-coveo-facets.js
@@ -6,12 +6,36 @@
 
   let tries = 500
 
+  const addVersionFacets = () => {
+    const atomicFacetManager = atomicFacetsLayoutSection.querySelector('atomic-facet-manager')
+    const components = window.siteNavigationData
+    components.forEach((component) => {
+      const versionFacet = createVersionFacet(component.name, component.title)
+      atomicFacetManager.appendChild(versionFacet)
+    })
+    delete window.siteNavigationData
+  }
+
+  const createVersionFacet = (name, title) => {
+    const versionFacet = document.createElement('atomic-facet')
+    versionFacet.setAttribute('depends-on-product', title)
+    versionFacet.setAttribute('facet-id', name)
+    versionFacet.setAttribute('field', 'mulesoftversionwithlatest')
+    versionFacet.setAttribute('heading-level', 2)
+    versionFacet.setAttribute('label', `${title} Version`)
+    versionFacet.setAttribute('number-of-value', 20)
+    versionFacet.setAttribute('sort-criteria', 'score')
+    return versionFacet
+  }
+
   const updateFacetTexts = (facetDiv) => {
     if (facetDiv) {
       const facetInput = facetDiv.querySelector('input')
       if (facetInput) facetInput.placeholder = 'Search Products'
     }
   }
+
+  addVersionFacets()
 
   const updateFacet = setInterval(() => {
     if (--tries <= 0) clearInterval(updateFacet)
