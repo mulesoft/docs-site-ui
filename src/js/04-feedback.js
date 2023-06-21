71ce171cbbb4700b6f62439933656051250e5341
Gary Cheung <gcheung@mulesoft.com>
2023-06-21 15:45:08 -0700

    force content type

--- src/js/04-feedback.js
+++ src/js/04-feedback.js
@@ -142,18 +142,22 @@
   const createGUSWorkItem = (form) => {
     const body = createBody(form)
 
-    fetch('/api/v1/form-submit', { method: 'POST', body }) // eslint-disable-line
-      .then((response) => {
-        if (response.ok) {
-          console.log('Form submitted successfully')
-          console.log(response)
-        } else {
-          console.error('Error submitting form')
-        }
-      })
-      .catch((error) => {
-        console.error('Network error:', error)
-      })
+    fetch('/api/v1/form-submit', { // eslint-disable-line
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    }).then((response) => {
+      if (response.ok) {
+        console.log('Form submitted successfully')
+        console.log(response)
+      } else {
+        console.error('Error submitting form')
+      }
+    }).catch((error) => {
+      console.error('Network error:', error)
+    })
   }
 
   const focusOnFirstInvalidInput = (feedbackForm) => {
