9beb31b085a0ee4ff1ab64d231cd06be61d3a9a5
Gary Cheung <gcheung@mulesoft.com>
2023-06-23 14:02:26 -0700

    upgrade prettier eslint

--- gulp.d/lib/gulp-prettier-eslint.js
+++ gulp.d/lib/gulp-prettier-eslint.js
@@ -23,14 +23,14 @@ module.exports = () => {
     }
   })
 
-  function format (file, enc, next) {
+  async function format (file, _enc, next) {
     if (file.isNull()) return next()
     if (file.isStream()) {
       return next(new PluginError('gulp-prettier-eslint', 'Streaming not supported'))
     }
 
     const input = file.contents.toString()
-    const output = prettierEslint({
+    const output = await prettierEslint({
       text: input,
       filePath: file.path,
     })
@@ -39,6 +39,7 @@ module.exports = () => {
       report.unchanged += 1
     } else {
       report.changed += 1
+      console.log(output)
       file.contents = Buffer.from(output)
     }
 
