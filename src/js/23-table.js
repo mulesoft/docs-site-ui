cc67755a962902fa690930404fe29f78065c01d0
Gary Cheung <gcheung@mulesoft.com>
2023-06-26 11:38:49 -0700

    add logic for adjusting col width

--- src/js/23-table.js
+++ src/js/23-table.js
@@ -9,17 +9,41 @@
     })
   }
 
+  const addDataHeaders = (table) => {
+    const tableHeaderTexts = getTableHeaderTexts(table)
+    const tableRows = table.querySelectorAll('tr')
+    tableRows.forEach((tableRow) => addDataHeader(tableRow, tableHeaderTexts))
+  }
+
+  const adjustMobileColumnWidth = (tableColumns, widthMap, e) => {
+    if (e) e.preventDefault()
+    tableColumns.forEach((col, index) => {
+      const colWidth = isBigScreenSize() ? widthMap[index] : '100%'
+      setColWidth(col, colWidth)
+    })
+  }
+
+  const getTableColumnsWidthMap = (tableColumns) => {
+    const columnsWidthMap = {}
+    tableColumns.forEach((col, index) => { columnsWidthMap[index] = col.style.width })
+    return columnsWidthMap
+  }
+
   const getTableHeaderTexts = (table) => {
     const tableHeader = table.querySelectorAll('th')
     const tableHeaderArray = Array.from(tableHeader)
     return tableHeaderArray.map((node) => node.innerText)
   }
 
-  const tables = document.querySelectorAll('table')
+  const isBigScreenSize = () => window.matchMedia(' (min-width: 768px)').matches
 
-  tables.forEach((table) => {
-    const tableHeaderTexts = getTableHeaderTexts(table)
-    const tableRows = table.querySelectorAll('tr')
-    tableRows.forEach((tableRow) => addDataHeader(tableRow, tableHeaderTexts))
-  })
+  const setColWidth = (col, width) => { col.style.width = width }
+
+  const tables = document.querySelectorAll('table')
+  tables.forEach((table) => addDataHeaders(table))
+
+  const tableColumns = document.querySelectorAll('table > colgroup > col')
+  const tableColumnsWidthMap = getTableColumnsWidthMap(tableColumns)
+  window.addEventListener('resize', (e) => adjustMobileColumnWidth(tableColumns, tableColumnsWidthMap, e))
+  adjustMobileColumnWidth(tableColumns, tableColumnsWidthMap)
 })()
