de62194328fda08b650cdaa75d3c98ea41e12455
Gary Cheung <gcheung@mulesoft.com>
2023-06-27 09:21:33 -0700

    improve image margin

--- src/js/17-image-margin.js
+++ src/js/17-image-margin.js
@@ -1,15 +1,27 @@
 ;(() => {
   'use strict'
 
-  const isHomePage = (pathname) => /(?:.*\/general\/|^\/$)/.test(pathname)
+  const isHomePage = (pathname) => ['/general/', '/'].includes(pathname)
 
   if (!isHomePage(window.location.pathname)) {
     const addTopAndBottomMargins = (element) => element.classList.add('page-images')
+
+    const firstElementInTableCell = (imageElement) => {
+      const imageParentSpan = imageElement.parentElement
+      const imageParentSpanParent = imageParentSpan.parentElement
+      return (
+        imageParentSpan === imageParentSpanParent.firstElementChild &&
+        imageParentSpanParent.classList.contains('tableblock')
+      )
+    }
+
     const isBig = (image) => image.height > 40
 
-    const pageImages = document.querySelectorAll('.doc img')
+    const pageImages = document.querySelectorAll(
+      '.doc img:not(.anchor-image):not(.external-link-image):not(.select-dropdown-arrow):not(.code-snippet-icon)'
+    )
     pageImages.forEach((image) => {
-      if (isBig(image)) addTopAndBottomMargins(image)
+      if (isBig(image) && !firstElementInTableCell(image)) addTopAndBottomMargins(image)
     })
   }
 })()
