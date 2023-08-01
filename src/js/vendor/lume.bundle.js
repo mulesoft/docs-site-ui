245ef5421a600e626f81dac5cd9c52fb60d6c5e2
Gary Cheung <gcheung@mulesoft.com>
2023-08-01 16:41:54 -0700

    add CDN link for future ref

--- src/js/vendor/lume.bundle.js
+++ src/js/vendor/lume.bundle.js
@@ -9,7 +9,8 @@
   // https://github.com/mulesoft/lume-design-system/blob/master/packages/lume-icons/README.md
   // https://lume.mulesoft.com/?path=/story/introduction-using-icon--page
   const test = document.createElement('lume-icon')
-  // test.setAttribute('set', 'classic')
+  test.setAttribute('set', 'classic')
+  test.setAttribute('href', 'https://cdn.anypoint.mulesoft.com')
   test.setAttribute('symbol', 'access-management-color')
   const doc = document.querySelector('article.doc')
   doc.insertBefore(test, doc.firstChild)
