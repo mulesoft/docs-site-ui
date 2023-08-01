9c8c1965d387dd8d461d0374777f71fc8fef3cc4
Gary Cheung <gcheung@mulesoft.com>
2023-08-01 16:27:55 -0700

    WIP add lume JS

---
+++ src/js/vendor/lume.bundle.js
@@ -0,0 +1,17 @@
+;(() => {
+  const icon = require('@mulesoft/lume-icon')
+  const customElements = window.customElements
+  if (!customElements.get('lume-icon')) {
+    customElements.define('lume-icon', icon)
+  }
+
+  // testing with the following block
+  // https://github.com/mulesoft/lume-design-system/blob/master/packages/lume-icons/README.md
+  // https://lume.mulesoft.com/?path=/story/introduction-using-icon--page
+  const test = document.createElement('lume-icon')
+  // test.setAttribute('set', 'classic')
+  test.setAttribute('symbol', 'access-management-color')
+  const doc = document.querySelector('article.doc')
+  doc.insertBefore(test, doc.firstChild)
+  // <lume-icon set="classic" symbol="access-management-color"></lume-icon>
+})()
