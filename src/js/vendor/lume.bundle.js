20d6d89baa1ac4714d4a49dbd5edf484d48af220
Gary Cheung <gcheung@mulesoft.com>
2023-08-01 16:42:46 -0700

    stage lume icon

--- src/js/vendor/lume.bundle.js
+++ src/js/vendor/lume.bundle.js
@@ -5,14 +5,13 @@
     customElements.define('lume-icon', icon)
   }
 
-  // testing with the following block
-  // https://github.com/mulesoft/lume-design-system/blob/master/packages/lume-icons/README.md
-  // https://lume.mulesoft.com/?path=/story/introduction-using-icon--page
-  const test = document.createElement('lume-icon')
-  test.setAttribute('set', 'classic')
-  test.setAttribute('href', 'https://cdn.anypoint.mulesoft.com')
-  test.setAttribute('symbol', 'access-management-color')
-  const doc = document.querySelector('article.doc')
-  doc.insertBefore(test, doc.firstChild)
-  // <lume-icon set="classic" symbol="access-management-color"></lume-icon>
+  // // testing with the following block
+  // // https://github.com/mulesoft/lume-design-system/blob/master/packages/lume-icons/README.md
+  // // https://lume.mulesoft.com/?path=/story/introduction-using-icon--page
+  // const test = document.createElement('lume-icon')
+  // test.setAttribute('set', 'classic')
+  // test.setAttribute('href', 'https://cdn.anypoint.mulesoft.com')
+  // test.setAttribute('symbol', 'access-management-color')
+  // const doc = document.querySelector('article.doc')
+  // doc.insertBefore(test, doc.firstChild)
 })()
