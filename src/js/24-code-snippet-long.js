d8d05debf147b4e3fa31e7bf35693cc57fc829fe
Gary Cheung <gcheung@mulesoft.com>
2023-08-15 15:13:29 -0700

    set max height

--- src/js/24-code-snippet-long.js
+++ src/js/24-code-snippet-long.js
@@ -8,7 +8,7 @@
   const tallerThan = (element, length) => element.getBoundingClientRect().height > length
   const tooTall = (element) => tallerThan(element, 166)
 
-  const codeSnippets = document.querySelectorAll('div.listingblock:has(pre)')
+  const codeSnippets = document.querySelectorAll('pre')
   codeSnippets.forEach((codeSnippet) => {
     if (tooTall(codeSnippet)) {
       collapse(codeSnippet)
