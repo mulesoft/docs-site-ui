543ab4da3476c66733a3be5c257e4d00af69d1a5
Gary Cheung <gcheung@mulesoft.com>
2023-08-15 15:08:44 -0700

    add code snippet script

---
+++ src/js/24-code-snippet-long.js
@@ -0,0 +1,17 @@
+;(() => {
+  'use strict'
+
+  const collapse = (codeSnippet) => {
+    codeSnippet.classList.add('collapsed')
+  }
+
+  const tallerThan = (element, length) => element.getBoundingClientRect().height > length
+  const tooTall = (element) => tallerThan(element, 166)
+
+  const codeSnippets = document.querySelectorAll('div.listingblock:has(pre)')
+  codeSnippets.forEach((codeSnippet) => {
+    if (tooTall(codeSnippet)) {
+      collapse(codeSnippet)
+    }
+  })
+})()
