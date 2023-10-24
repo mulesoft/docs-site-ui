7f4e5972c52a1c0d8a43c4dbabd8ccebaf68d8e4
jdomingu <jared.dominguez@salesforce.com>
2023-10-24 15:16:23 -0700

    Refactor

--- src/js/00-globals.js
+++ src/js/00-globals.js
@@ -1,65 +1,69 @@
 'use strict'
 
-window.MSCX = window.MSCX || {
-  l10n: {
-    fallbackLocale: 'en',
-    messages: 'includeLocMessagesAtBuildtime',
-  },
-}
+window.MSCX = window.MSCX || {}
 
-MSCX.l10n.setLocale = function (lang) {
-  const docLang = document.documentElement.lang
+MSCX.l10n = (function () {
+  const messageSets = 'includeLocMessagesAtBuildtime'
+  const fallbackLocale = 'en'
+  let messages = {}
+  let locale = ''
 
-  if (Object.hasOwn(this.messages, lang)) {
-    this.locale = lang
-  } else if (Object.hasOwn(this.messages, docLang)) {
-    this.locale = docLang
-  } else {
-    this.locale = this.fallbackLocale
-  }
+  function setLocale (lang) {
+    const docLang = document.documentElement.lang
 
-  this.message = this.messages[this.locale]
-  if (this.locale !== this.fallbackLocale) {
-    Object.setPrototypeOf(this.message, this.messages[this.fallbackLocale])
-  }
-}
-
-MSCX.l10n.getMessage = function (messageKey) {
-  if (!(messageKey in this.message)) {
-    console.error(`Missing UI string: ${messageKey}`)
-    return messageKey
-  }
-
-  return this.message[messageKey]
-}
-
-MSCX.l10n.localizeDOM = function () {
-  const loadingElems = document.querySelectorAll('.hide-until-l10n')
-
-  MSCX.l10n.localizeAttribute('data-l10n-text', 'textContent')
-  MSCX.l10n.localizeAttribute('data-l10n-label', 'aria-label')
-  // Optionally, hide elements until loaded to prevent FOUC
-  loadingElems.forEach((elem) => {
-    elem.classList.remove('hide-until-l10n')
-  })
-}
-
-MSCX.l10n.localizeAttribute = function (attributeSrc, attributeTarget) {
-  let locMsg = ''
-  const elems = document.querySelectorAll(`[${attributeSrc}]`)
-
-  elems.forEach((elem) => {
-    locMsg = MSCX.l10n.getMessage(elem.getAttribute(attributeSrc))
-    if (attributeTarget === 'textContent') {
-      elem.textContent = locMsg
+    if (Object.hasOwn(messageSets, lang)) {
+      locale = lang
+    } else if (Object.hasOwn(messageSets, docLang)) {
+      locale = docLang
     } else {
-      elem.setAttribute(attributeTarget, locMsg)
+      locale = fallbackLocale
     }
-  })
-}
 
-MSCX.l10n.init = (function () {
-  Object.freeze(MSCX.l10n.messages)
-  MSCX.l10n.setLocale()
-  window.addEventListener('load', MSCX.l10n.localizeDOM)
+    messages = messageSets[locale]
+    if (locale !== fallbackLocale) {
+      Object.setPrototypeOf(messages, messageSets[fallbackLocale])
+    }
+  }
+
+  function lookupMessage(messageKey) {
+    if (!(messageKey in messages)) {
+      console.error(`Missing UI string: ${messageKey}`)
+      return messageKey
+    }
+
+    return messages[messageKey]
+  }
+
+  function localizeDOM () {
+    const loadingElems = document.querySelectorAll('.hide-until-l10n')
+
+    localizeAttribute('data-l10n-text', 'textContent')
+    localizeAttribute('data-l10n-label', 'aria-label')
+    // Optionally, hide elements until loaded to prevent FOUC
+    loadingElems.forEach((elem) => {
+      elem.classList.remove('hide-until-l10n')
+    })
+  }
+
+  function localizeAttribute (attributeSrc, attributeTarget) {
+    let locMsg = ''
+    const elems = document.querySelectorAll(`[${attributeSrc}]`)
+
+    elems.forEach((elem) => {
+      locMsg = lookupMessage(elem.getAttribute(attributeSrc))
+      if (attributeTarget === 'textContent') {
+        elem.textContent = locMsg
+      } else {
+        elem.setAttribute(attributeTarget, locMsg)
+      }
+    })
+  }
+
+  Object.freeze(messageSets)
+  setLocale()
+  window.addEventListener('load', localizeDOM)
+
+  return {
+    getMessage: lookupMessage
+  }
 })()
