0c1ee638c50aa756d1fa8835f489fb0d76c63b42
jdomingu <jared.dominguez@salesforce.com>
2023-10-24 12:24:24 -0700

    Initial ui string extraction and loc lib

---
+++ src/js/00-l10n.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var l10n = l10n || {
+  fallbackLocale: 'en',
+  messages: {
+    'en': {
+      'copy-link-copied-text': 'Copied',
+      'copy-link-hover-text': 'Copy link to clipboard',
+      'right-nav-on-this-page': 'On this page:',
+      'right-nav-skip-to-main': 'Skip to main content',
+      'right-nav-skip-to-sections': 'Skip to page content sections'
+      },
+    'ja': {
+      'copy-link-hover-text': 'リンクをクリップボードにコピー',
+      'right-nav-on-this-page': 'このページの内容:'
+    }
+  }
+};
+
+l10n.setLocale = function (lang) {
+  let docLang = document.documentElement.lang;
+
+  if (this.messages.hasOwnProperty(lang)) {
+    this.locale = lang;
+  } else if (this.messages.hasOwnProperty(docLang)) {
+    this.locale = docLang;
+  } else {
+    this.locale = this.fallbackLocale;
+  }
+
+  this.message = this.messages[this.locale];
+  if (this.locale !== this.fallbackLocale) {
+    Object.setPrototypeOf(this.message, this.messages[this.fallbackLocale]);
+  }
+};
+
+l10n.getMessage = function (messageKey) {
+  if (!messageKey in this.message) {
+    console.error(`Missing UI string: ${messageKey}`)
+    return messageKey;
+  }
+
+  return this.message[messageKey];
+}
+
+l10n.localizeDOM = function() {
+  let loadingElems = document.querySelectorAll('.hide-until-l10n');
+
+  l10n.localizeAttribute('data-l10n-text', 'textContent');
+  l10n.localizeAttribute('data-l10n-label', 'aria-label');
+  // Optionally, hide elements until loaded to prevent FOUC
+  loadingElems.forEach((elem) => {
+    elem.classList.remove('hide-until-l10n')
+  })
+};
+
+l10n.localizeAttribute = function(attributeSrc, attributeTarget) {
+  let locMsg = '';
+  let elems = document.querySelectorAll(`[${attributeSrc}]`);
+
+  elems.forEach((elem) => {
+    locMsg = l10n.getMessage(elem.getAttribute(attributeSrc));
+    if (attributeTarget === 'textContent') {
+      elem.textContent = locMsg;
+    } else {
+      elem.setAttribute(attributeTarget, locMsg);
+    }
+  });
+};
+
+l10n.init = function() {
+  Object.freeze(l10n.messages);
+  l10n.setLocale('ja');
+  window.addEventListener('load', l10n.localizeDOM);
+}();
