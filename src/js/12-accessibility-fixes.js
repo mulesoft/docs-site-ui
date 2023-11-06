dd035bc7b18d2be41048280b095f2e9254e6f36f
jdomingu <jared.dominguez@salesforce.com>
2023-11-06 12:21:44 -0800

    Add aria-expanded to details elements

--- src/js/12-accessibility-fixes.js
+++ src/js/12-accessibility-fixes.js
@@ -8,5 +8,27 @@
     })
   }
 
+  const onExpand = (e) => {
+    const elem = e.target
+    setAriaExpanded(elem)
+  }
+
+  const setAriaExpanded = (elem) => {
+    if (elem.hasAttribute('open')) {
+      elem.setAttribute('aria-expanded', true)
+      return
+    }
+    elem.setAttribute('aria-expanded', false)
+  }
+
+  const setDetailsExpandListeners = () => {
+    const detailsElems = document.querySelectorAll('details')
+    detailsElems.forEach((detail) => {
+      setAriaExpanded(detail)
+      detail.addEventListener('toggle', onExpand)
+    })
+  }
+
   addBlankAltTextsToTipIcons()
+  setDetailsExpandListeners()
 })()
