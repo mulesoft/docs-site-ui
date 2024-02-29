74031c573b6519545baacee94646213202dcdbab
Gary Cheung <gcheung@mulesoft.com>
2024-02-29 11:27:48 -0800

    no fetch if anchor is on the same page

--- src/js/24-add-title.js
+++ src/js/24-add-title.js
@@ -11,31 +11,34 @@
 
   const populateTitle = (a) => {
     if (a.hasAttribute('title')) return
-    if (hasSameOrigin(a.href) && !isAnchor(a.href)) {
-      const anchor = getAnchor(a.href)
-      fetch(a.href).then((response) => {
-        return response.text().then((html) => {
-          /* eslint-disable no-undef */
-          const pageHTML = new DOMParser()
-            /* eslint-enable no-undef */
-            .parseFromString(html, 'text/html')
-          let desc
-          if (anchor) {
-            const anchorElement = pageHTML.querySelector(`#${anchor}`)?.nextElementSibling
-            if (anchorElement) {
-              desc =
-                anchorElement.tagName === 'P' ? anchorElement.textContent : anchorElement.querySelector('p').textContent
-            }
-          } else {
-            desc = pageHTML.querySelector('#preamble p, article p').textContent
-          }
-          let titleText = desc.replace(/\n/g, ' ').replace(/ +/g, ' ').trim()
-          if (desc) {
-            titleText = titleText.length > MAX_TITLE_LENGTH ? `${titleText.substring(0, 512)}...` : titleText
-            a.setAttribute('title', titleText)
-          }
+    if (hasSameOrigin(a.href)) {
+      let anchor
+      let desc
+
+      if (isAnchor(a.href)) anchor = getAnchor(a.href)
+
+      if (anchor) {
+        const anchorElement = document.querySelector(`#${anchor}`)?.nextElementSibling
+        if (anchorElement) {
+          desc =
+            anchorElement.tagName === 'P' ? anchorElement.textContent : anchorElement.querySelector('p').textContent
+        }
+      } else {
+        fetch(a.href).then((response) => {
+          return response.text().then((html) => {
+            /* eslint-disable no-undef */
+            desc = new DOMParser()
+              /* eslint-enable no-undef */
+              .parseFromString(html, 'text/html')
+              .querySelector('#preamble p, article p').textContent
+          })
         })
-      })
+      }
+      let titleText = desc.replace(/\n/g, ' ').replace(/ +/g, ' ').trim()
+      if (desc) {
+        titleText = titleText.length > MAX_TITLE_LENGTH ? `${titleText.substring(0, 512)}...` : titleText
+        a.setAttribute('title', titleText)
+      }
     }
   }
 
