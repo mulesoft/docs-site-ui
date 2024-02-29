7b2dcb7e89851cc31dd3edef1369434d6661a398
Gary Cheung <gcheung@mulesoft.com>
2024-02-29 13:37:46 -0800

    temporarily set facets in search-interface

--- src/js/21-coveo-facets.js
+++ src/js/21-coveo-facets.js
@@ -1,74 +1,74 @@
-;(() => {
-  'use strict'
+// ;(() => {
+//   'use strict'
 
-  const atomicFacetsLayoutSection = document.querySelector('atomic-layout-section[section="facets"]')
-  if (!atomicFacetsLayoutSection) return
+//   const atomicFacetsLayoutSection = document.querySelector('atomic-layout-section[section="facets"]')
+//   if (!atomicFacetsLayoutSection) return
 
-  let tries = 500
+//   let tries = 500
 
-  const addVersionFacets = () => {
-    const components = window.siteNavigationData
-    components.forEach((component) => {
-      const versionFacet = createVersionFacet(component.name, component.title, component.versions)
-      if (versionFacet) atomicFacetsLayoutSection.appendChild(versionFacet)
-    })
-    delete window.siteNavigationData
-  }
+//   const addVersionFacets = () => {
+//     const components = window.siteNavigationData
+//     components.forEach((component) => {
+//       const versionFacet = createVersionFacet(component.name, component.title, component.versions)
+//       if (versionFacet) atomicFacetsLayoutSection.appendChild(versionFacet)
+//     })
+//     delete window.siteNavigationData
+//   }
 
-  const createVersionFacet = (componentID, componentDisplayName, versions) => {
-    if (isSearchable(componentID)) {
-      const formattedVersions = getFormattedDisplayVersions(versions)
-      if (formattedVersions) {
-        const versionFacet = document.createElement('atomic-facet')
-        versionFacet.setAttribute('allowed-values', formattedVersions)
-        versionFacet.setAttribute('depends-on-product', componentDisplayName)
-        versionFacet.setAttribute('facet-id', `${componentID}-version`)
-        versionFacet.setAttribute('field', 'mulesoftversionwithlatest')
-        versionFacet.setAttribute('heading-level', 2)
-        versionFacet.setAttribute('label', `${componentDisplayName} Version`)
-        versionFacet.setAttribute('number-of-value', 20)
-        versionFacet.setAttribute('sort-criteria', 'score')
-        versionFacet.setAttribute('with-search', false)
+//   const createVersionFacet = (componentID, componentDisplayName, versions) => {
+//     if (isSearchable(componentID)) {
+//       const formattedVersions = getFormattedDisplayVersions(versions)
+//       if (formattedVersions) {
+//         const versionFacet = document.createElement('atomic-facet')
+//         versionFacet.setAttribute('allowed-values', formattedVersions)
+//         versionFacet.setAttribute('depends-on-product', componentDisplayName)
+//         versionFacet.setAttribute('facet-id', `${componentID}-version`)
+//         versionFacet.setAttribute('field', 'mulesoftversionwithlatest')
+//         versionFacet.setAttribute('heading-level', 2)
+//         versionFacet.setAttribute('label', `${componentDisplayName} Version`)
+//         versionFacet.setAttribute('number-of-value', 20)
+//         versionFacet.setAttribute('sort-criteria', 'score')
+//         versionFacet.setAttribute('with-search', false)
 
-        return versionFacet
-      }
-    }
-  }
+//         return versionFacet
+//       }
+//     }
+//   }
 
-  const getFormattedDisplayVersions = (versions) => {
-    const listOfVersions = versions.map((t) => t.displayVersion || t.version)
-    if (listOfVersions.length && isNumberedVersion(listOfVersions[0])) {
-      const latest = MSCX.l10n.getMessage('latest')
-      listOfVersions[0] += ` [${latest}]`
-      return `["${listOfVersions.slice(0, 25).join('", "')}"]`
-    }
-  }
+//   const getFormattedDisplayVersions = (versions) => {
+//     const listOfVersions = versions.map((t) => t.displayVersion || t.version)
+//     if (listOfVersions.length && isNumberedVersion(listOfVersions[0])) {
+//       const latest = MSCX.l10n.getMessage('latest')
+//       listOfVersions[0] += ` [${latest}]`
+//       return `["${listOfVersions.slice(0, 25).join('", "')}"]`
+//     }
+//   }
 
-  const isNumberedVersion = (versionName) => versionName && !['default', 'latest', 'master'].includes(versionName)
-  const isSearchable = (componentName) => !['general', 'reuse'].includes(componentName)
+//   const isNumberedVersion = (versionName) => versionName && !['default', 'latest', 'master'].includes(versionName)
+//   const isSearchable = (componentName) => !['general', 'reuse'].includes(componentName)
 
-  const updateFacetTexts = (facetDiv) => {
-    if (facetDiv) {
-      const facetInput = facetDiv.querySelector('input')
-      if (facetInput) facetInput.placeholder = 'Search Products'
-    }
-  }
+//   const updateFacetTexts = (facetDiv) => {
+//     if (facetDiv) {
+//       const facetInput = facetDiv.querySelector('input')
+//       if (facetInput) facetInput.placeholder = 'Search Products'
+//     }
+//   }
 
-  addVersionFacets()
+//   addVersionFacets()
 
-  const updateFacet = setInterval(() => {
-    if (--tries <= 0) clearInterval(updateFacet)
+//   const updateFacet = setInterval(() => {
+//     if (--tries <= 0) clearInterval(updateFacet)
 
-    const atomicFacet = document.querySelector('atomic-facet')
-    const facetShadowRoot = atomicFacet?.shadowRoot
-    const firstFacetDiv = facetShadowRoot?.querySelector('div[part="facet"]')
-    if (firstFacetDiv) {
-      try {
-        updateFacetTexts(firstFacetDiv)
-        clearInterval(updateFacet)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-  }, 100)
-})()
+//     const atomicFacet = document.querySelector('atomic-facet')
+//     const facetShadowRoot = atomicFacet?.shadowRoot
+//     const firstFacetDiv = facetShadowRoot?.querySelector('div[part="facet"]')
+//     if (firstFacetDiv) {
+//       try {
+//         updateFacetTexts(firstFacetDiv)
+//         clearInterval(updateFacet)
+//       } catch (error) {
+//         console.error(error)
+//       }
+//     }
+//   }, 100)
+// })()
