edcd626101da6fe894612701030007cdba2a5c6e
jdomingu <jared.dominguez@salesforce.com>
2024-04-12 12:37:45 -0700

    Wrap JP Xrefs in Bracket Quotes

---
+++ src/js/24-jp-processing.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-var */
+;(() => {
+  'use strict'
+
+  if (MSCX.l10n.getLocale() !== 'jp') {
+    return
+  }
+
+  const xrefLinks = document.querySelectorAll('article a.xref')
+
+  function linkContainsText (linkElement) {
+    const hasText = linkElement.textContent.trim().length > 0
+    return hasText
+  }
+
+  function linkIsOnlyContentInParent (linkElement) {
+    const parentElement = linkElement.parentElement
+
+    if (parentElement) {
+      const childNodes = parentElement.childNodes
+      return childNodes.length === 1 && childNodes[0] === linkElement
+    }
+
+    return false
+  }
+
+  // Does not work in preview builds, only full builds include xref class above
+  function wrapXrefLinksInJpBrackets () {
+    xrefLinks.forEach((link) => {
+      const linkUrl = new URL(link.href)
+
+      if (!linkContainsText(link)) return
+      if (linkIsOnlyContentInParent(link)) return
+
+      if (linkUrl.hash) {
+        link.innerHTML = `「${link.innerHTML}」`
+      } else {
+        link.innerHTML = `『${link.innerHTML}』`
+      }
+    })
+  }
+
+  wrapXrefLinksInJpBrackets()
+})()
