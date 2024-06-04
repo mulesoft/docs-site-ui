08ec2ff3145c8c3e5eee2c6eb86179259909ca7c
Gary Cheung <gcheung@mulesoft.com>
2024-06-04 11:02:09 -0700

    add fallback links

--- src/js/25-trending-topics-fallback.js
+++ src/js/25-trending-topics-fallback.js
@@ -3,25 +3,45 @@
 
   if (!document.querySelector('atomic-recs-interface')) return
 
+  const addFallbackLinks = (parentDiv) => {
+    const trendingTopicsFallback = 'trendingTopicsFallback'
+    const title = document.createElement('h2')
+    title.innerText = trendingTopicsFallback[document.documentElement.lang].title
+    parentDiv.appendChild(title)
+    trendingTopicsFallback[document.documentElement.lang].links.forEach((link) => {
+      const div = document.createElement('div')
+      const a = document.createElement('a')
+      a.innerText = link.name
+      a.setAttribute('href', link.url)
+      a.classList.add('trending-topics-hardcoded')
+      parentDiv.appendChild(div)
+      div.appendChild(a)
+    })
+  }
+
+  const hasValidRecommendations = (recsListShadowRoot) => {
+    if (recsListShadowRoot.childElementCount === 0) return false
+    const atomicRecsResult = recsListShadowRoot.querySelector('atomic-recs-result')
+    if (atomicRecsResult) {
+      const recsResultShadowRoot = atomicRecsResult.shadowRoot
+      const atomicText = recsResultShadowRoot.querySelector('atomic-text')
+      return atomicText.value !== 'no-title'
+    }
+    return true
+  }
+
   setTimeout(() => {
     const atomicRecsList = document.querySelector('atomic-recs-list')
     const recsListShadowRoot = atomicRecsList?.shadowRoot
     if (recsListShadowRoot) {
       try {
-        if (recsListShadowRoot.childElementCount === 0) {
-          const trendingTopicsFallback = 'trendingTopicsFallback'
-          // TODO: add title
-          trendingTopicsFallback[document.documentElement.lang].links.forEach((link) => {
-            const a = document.createElement('a')
-            a.innerText = link.name
-            a.setAttribute('href', link.url)
-            a.classList.add('trending-topics-hardcoded')
-            atomicRecsList.parentElement.parentElement.appendChild(a)
-          })
+        if (!hasValidRecommendations(recsListShadowRoot)) {
+          addFallbackLinks(atomicRecsList.parentElement.parentElement)
+          atomicRecsList.remove()
         }
       } catch (error) {
         console.error(error)
       }
     }
-  }, 5000)
+  }, 4000)
 })()
