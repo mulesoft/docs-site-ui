cf1bcaa11441a132be8847a1690ce14ad5e5aa00
jdomingu <jared.dominguez@salesforce.com>
2024-06-14 13:05:13 -0700

    Fix: Archive connectors landing page missing connectors

--- src/helpers/connectors.js
+++ src/helpers/connectors.js
@@ -5,7 +5,32 @@ module.exports = function ({
     root: { site },
   },
 }) {
-  return Object.values(site.components).filter(
-    ({ asciidoc = { attributes: {} } }) => asciidoc.attributes['page-connector-type']
-  )
+  function getLocalConnectorComponents (components) {
+    const connectors = {}
+
+    for (const component in components) {
+      if (component.endsWith('-connector')) {
+        const connector = components[component]
+        // Filter out connector versions that are missing the page-connector-type attribute.
+        // These are imported from the site manifest with the @antora/atlas-extension in the playbook,
+        // and should not display in the build.
+        const filteredVersions = connector.versions.filter(
+          (version) => version.asciidoc.attributes['page-connector-type']
+        )
+
+        if (filteredVersions.length === 0) {
+          continue
+        }
+
+        // Replace latest with a local version, rather than the latest imported version
+        connector.latest = filteredVersions[0]
+        connector.versions = filteredVersions
+        connectors[component] = connector
+      }
+    }
+
+    return connectors
+  }
+
+  return getLocalConnectorComponents(site.components)
 }
