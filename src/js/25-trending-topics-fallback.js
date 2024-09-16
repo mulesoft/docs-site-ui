0070180d751c480a03c100f706aa7780b0f13be3
Gary Cheung <gcheung@mulesoft.com>
2024-09-16 11:15:28 -0700

    [W-16713536] migrate Coveo to UAT (#792)
    * migrate UA to salesforce-dev
    
    * Bump serve-static from 1.15.0 to 1.16.2
    
    Bumps [serve-static](https://github.com/expressjs/serve-static) from 1.15.0 to 1.16.2.
    - [Release notes](https://github.com/expressjs/serve-static/releases)
    - [Changelog](https://github.com/expressjs/serve-static/blob/v1.16.2/HISTORY.md)
    - [Commits](https://github.com/expressjs/serve-static/compare/v1.15.0...v1.16.2)
    
    ---
    updated-dependencies:
    - dependency-name: serve-static
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * handle error if element doesn't exist
    
    * vuln patch
    
    * upgrade coveo ver
    
    * revert - open page in same tab
    
    * update search hub
    
    * keep content recommendations to old org
    
    * update search hub
    
    * switch to UAT
    
    * no more hardcoding token
    
    * forcing fallback links for now until migration is done
    
    * comment out unused func for now
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/js/25-trending-topics-fallback.js
+++ src/js/25-trending-topics-fallback.js
@@ -10,26 +10,26 @@
     }
   }
 
-  const hasValidRecommendations = (recsListShadowRoot) => {
-    if (recsListShadowRoot.childElementCount === 0) return false
-    const atomicRecsResult = recsListShadowRoot.querySelector('atomic-recs-result')
-    if (atomicRecsResult) {
-      const recsResultShadowRoot = atomicRecsResult.shadowRoot
-      const atomicText = recsResultShadowRoot.querySelector('atomic-text')
-      return atomicText?.value !== 'no-title'
-    }
-    return true
-  }
+  // const hasValidRecommendations = (recsListShadowRoot) => {
+  //   if (recsListShadowRoot.childElementCount === 0) return false
+  //   const atomicRecsResult = recsListShadowRoot.querySelector('atomic-recs-result')
+  //   if (atomicRecsResult) {
+  //     const recsResultShadowRoot = atomicRecsResult.shadowRoot
+  //     const atomicText = recsResultShadowRoot.querySelector('atomic-text')
+  //     return atomicText?.value !== 'no-title'
+  //   }
+  //   return true
+  // }
 
   setTimeout(() => {
     const atomicRecsList = document.querySelector('atomic-recs-list')
     const recsListShadowRoot = atomicRecsList?.shadowRoot
     if (recsListShadowRoot) {
       try {
-        if (!hasValidRecommendations(recsListShadowRoot)) {
-          addFallbackLinks(atomicRecsList.parentElement.parentElement)
-          atomicRecsList.parentElement.remove()
-        }
+        // if (!hasValidRecommendations(recsListShadowRoot)) {
+        addFallbackLinks(atomicRecsList.parentElement.parentElement)
+        atomicRecsList.parentElement.remove()
+        // }
       } catch (error) {
         console.error(error)
       }
