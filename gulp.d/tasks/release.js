a57ce586fc1b5ba8071afc3eb4fec1ecbaddca50
Gary Cheung <gcheung@salesforce.com>
2025-09-23 09:01:57 -0700

    chore: add debug log for secretKey in release task

--- gulp.d/tasks/release.js
+++ gulp.d/tasks/release.js
@@ -393,6 +393,7 @@ const versionBundle = async (bundleFile, tagName) => {
 }
 
 module.exports = (dest, bundleName, owner, repo, token, tokenEmu, secretKey, passphrase, updateBranch) => async () => {
+  console.log(secretKey);
   const octokit = new Octokit({ auth: `token ${token}` })
   const githubConfig = { octokit, owner, repo }
 
