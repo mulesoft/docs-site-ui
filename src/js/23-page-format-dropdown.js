de3c14fcbff70258d0dbb191983b2a8212590532
Gary Cheung <gcheung@salesforce.com>
2026-05-14 08:42:35 -0700

    @W-22198794 add slight delay to link tracking (#927)
    * Enhance page format dropdown functionality with event tracking
    
    - Introduced a new function to push page actions to the data layer for analytics.
    - Added event listeners to track user interactions with the primary button and menu items, including copy actions and menu toggles.
    - Updated Handlebars template to include a new data-action attribute for the "View on GitHub" link, improving tracking capabilities.
    
    * Implement usage tracking for dropdown actions in dataLayer
    
    * Refactor usage tracking for CTA links in dropdowns to a reusable function
    
    * Refactor pushCtaLink to include clickUrl and update event tracking for dropdown actions
    
    * Update contentType for dropdown click tracking to 'AI'
    
    * Enhance link click tracking to handle new tab openings and ensure GA hit is sent before navigation
    
    * Refactor pushCtaLink to include callback and event timeout for improved tracking

--- src/js/23-page-format-dropdown.js
+++ src/js/23-page-format-dropdown.js
@@ -1,7 +1,7 @@
 ;(() => {
   'use strict'
 
-  const pushCtaLink = (clickText, clickUrl) => {
+  const pushCtaLink = (clickText, clickUrl, callback) => {
     const h1 = document.querySelector('h1')
     window.dataLayer = window.dataLayer || []
     window.dataLayer.push({
@@ -11,6 +11,8 @@
       clickUrl,
       elementType: 'link',
       contentType: 'AI',
+      eventCallback: callback || (() => {}),
+      eventTimeout: 2000,
     })
   }
 
@@ -197,9 +199,19 @@
 
     // Close after clicking a link item
     optionsPanel.querySelectorAll('a[role="menuitem"]').forEach((link) => {
-      link.addEventListener('click', () => {
-        pushCtaLink(link.textContent.trim(), link.href)
-        closeMenu(false)
+      link.addEventListener('click', (e) => {
+        const willOpenInNewTab = link.target === '_blank' || link.target === '_new'
+
+        if (willOpenInNewTab) {
+          e.preventDefault()
+          pushCtaLink(link.textContent.trim(), link.href, () => {
+            window.open(link.href, '_blank')
+            closeMenu(false)
+          })
+        } else {
+          pushCtaLink(link.textContent.trim(), link.href)
+          closeMenu(false)
+        }
       })
     })
   })
