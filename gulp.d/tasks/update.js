bcc08b3110a15f95c206c25f06c8374ee1abb754
Gary Cheung <gcheung@salesforce.com>
2026-08-31 20:57:16 -0700

    Refactor updateContent function for improved readability and error handling

--- gulp.d/tasks/update.js
+++ gulp.d/tasks/update.js
@@ -17,15 +17,23 @@ module.exports = (partialsDir) => async () => {
 async function updateContent (partialsDir, component, contentType) {
   try {
     const urlParams = await getUrlParams(contentType)
-    const content = await fetch(
-      // https://salesforce.quip.com/lUuGAQD0svLd
-      `https://www.mulesoft.com/api/${component}?${urlParams}&docs-site&no-helmet&promotion-banner=true`
-    )
+    const url = `https://www.mulesoft.com/api/${component}?${urlParams}&docs-site&no-helmet&promotion-banner=true`
+    const content = await fetch(url, {
+      headers: {
+        Accept: 'application/json, text/html, */*',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 ' +
+          '(KHTML, like Gecko) Chrome/127.0.0.1 Safari/537.36',
+      },
+    })
     if (await isGoodStatus(content.status)) {
       const body = await content.json()
       if (await hasValidData(body)) {
         fs.writeFileSync(`${partialsDir}/${component}/${component}-content-${contentType}.hbs`, pretty(body.data))
       }
+    } else {
+      console.warn(`cannot fetch content right now. Please try again later. Status: ${content.status}`)
     }
   } catch (error) {
     console.warn(`cannot fetch content right now. Please try again later. Error: ${error}`)
@@ -33,9 +41,8 @@ async function updateContent (partialsDir, component, contentType) {
 }
 
 async function getUrlParams (contentType) {
-  if (languages.includes(contentType)) {
-    return `language=${contentType}&selector=true&selector_jp`
-  }
+  const selectorParams = 'selector=true&selector_jp'
+  return languages.includes(contentType) ? `language=${contentType}&${selectorParams}` : selectorParams
 }
 
 async function isGoodStatus (status) {
